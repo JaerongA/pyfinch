3f8e714074bde2ed8f12e7fdd7f52b7bccad2b12
JaerongAhn <jaerongahn@gmail.com>
2020-06-14 20:32:00 -0400

    Master script

---
+++ Summary_main.py
@@ -0,0 +1,36 @@
+import os
+import pandas as pd
+
+projectROOT = r'H:\Box\Data\Deafening Project'  # home
+# projectROOT = r'C:\Users\jahn02\Box\Data\Deafening Project'  # lab
+# projectROOT =r'C:\Users\AJR\Box\Data\Deafening Project'  # home
+
+
+summary_path = projectROOT + '\Analysis\Summary'
+summary = "Cluster_summary(Deafening).xlsx"
+
+os.chdir(summary_path)
+summary_cluster = pd.read_excel(summary)
+
+print('Loading the summary file')
+
+nb_cluster = summary_cluster.shape[0]
+
+for cluster_run in range(0, nb_cluster):
+
+
+    ## Read information from cluster summary
+    Key = str(summary_cluster['Key'][cluster_run])
+    if len(Key) == 1: Key = '0' + str(Key) # make the length equal
+
+    BirdID = summary_cluster['BirdID'][cluster_run]
+    TaskName = summary_cluster['TaskName'][cluster_run]
+    TaskSession = summary_cluster['TaskSession'][cluster_run]
+    TaskName = summary_cluster['TaskName'][cluster_run]
+    TaskName = summary_cluster['TaskName'][cluster_run]
+    TaskName = summary_cluster['TaskName'][cluster_run]
+    TaskName = summary_cluster['TaskName'][cluster_run]
+    TaskName = summary_cluster['TaskName'][cluster_run]
+
+
+    print(Key,BirdID)
