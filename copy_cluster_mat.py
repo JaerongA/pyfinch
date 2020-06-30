e2214e93b60080a9e2a343e29790016e059cf8a0
JaerongAhn <jaerongahn@gmail.com>
2020-06-29 22:17:18 -0400

    saves SpkInfo.mat to individual cell folders

--- copy_cluster_mat.py
+++ copy_cluster_mat.py
@@ -1,4 +1,10 @@
+# Created by JR (06/29/2020)
+# This program copy/pastes SpkInfo.mat from each cell root to the destinatoin folder (InformationAnalysis)
+
+import os
 from summary import load
+
+global projectROOT, saveROOT
 
 config_file = 'project.ini'
 parser = load.config(config_file)
@@ -6,25 +12,34 @@ projectROOT = load.project(parser)  # find the project folder
 summary_cluster, nb_cluster = load.summary(parser)  # load cluster summary file
 del config_file, parser
 
-def copy_cluster_mat(summary_cluster, projectROOT):
-
-    # for cluster_run in range(0, nb_cluster):
-    cluster_run = 0
-    cluster = load.cluster(summary_cluster, cluster_run)
-    # print(cluster)
-    sessionID, cellID, cellROOT = load.cluster_info(cluster)
+# Make a folder to save files
+saveROOT = projectROOT + '\\Analysis\\InformationAnalysis'
+if not os.path.exists(saveROOT):
+    os.mkdir(saveROOT)
 
 
-    return sessionID, cellID, cellROOT, cluster
+def copy_cluster_mat(summary_cluster):
+    import shutil
 
-    # pass
+    for cluster_run in range(0, nb_cluster):
+        cluster = load.cluster(summary_cluster, cluster_run)
 
+        if int(cluster.AnalysisOK):
+            # print(cluster)
+            sessionID, cellID, cellROOT = load.cluster_info(cluster)
+            print('Accessing... ' + cellROOT)
+            os.chdir(cellROOT)
 
+            mat_file = [file for file in os.listdir(cellROOT) if file.endswith('SpkInfo.mat')][0]
+            print(mat_file)
 
+            # Make a new folder for individual neurons
+            saveROOT2 = saveROOT + '\\' + cellID
+            if not os.path.exists(saveROOT2):
+                os.mkdir(saveROOT2)
 
-
-
+            shutil.copy(mat_file, saveROOT2)
 
 
 if __name__ == '__main__':
-    sessionID, cellID, cellROOT, cluster = copy_cluster_mat(summary_cluster, projectROOT)
+    copy_cluster_mat(summary_cluster)
