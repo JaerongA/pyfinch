475089f95bf0494f20d62fcdab6f22d9924da60b
JaerongAhn <jaerongahn@gmail.com>
2020-07-01 19:46:05 -0400

    save setting

---
+++ summary/save.py
@@ -0,0 +1,13 @@
+
+def make_save_dir(path_name: str, date_ok : str):
+
+    import os
+
+    # saveROOT = projectROOT + '\\Analysis\\InformationAnalysis'
+    # if not os.path.exists(saveROOT):
+    #     os.mkdir(saveROOT)
+    pass
+
+
+    # TODO: make a virtual environment
+
