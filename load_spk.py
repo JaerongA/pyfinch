cc426cbe23bb5e03ccadf71cfa7c8f858124dfea
JaerongAhn <jaerongahn@gmail.com>
2020-07-14 22:45:27 -0400

    plot the raw data for each channel

--- load_spk.py
+++ load_spk.py
@@ -1,25 +1,26 @@
 import os
-from summary.read_config import parser
 from summary import load
-from summary import save
+# from summary import save
 from load_intan_rhd_format.load_intan_rhd_format import read_data
+import matplotlib.pyplot as plt
 
-project_path = load.project(parser)  # find the project folder
-summary_cluster, nb_cluster = load.summary(parser)  # load cluster summary file
+summary_df, nb_cluster = load.summary(load.config())  # load cluster summary file
 
 for cluster_run in range(0, nb_cluster):
-    cluster = load.cluster(summary_cluster, cluster_run)
-    sessionID, cellID, cellROOT = load.cluster_info(cluster)
+    cluster = load.cluster(summary_df, cluster_run)
+    session_id, cell_id, session_path, cell_path = load.cluster_info(cluster)
 
-    print('Accessing... ' + cellROOT)
-    os.chdir(cellROOT)
-    rhd_files = [file for file in os.listdir(cellROOT) if file.endswith('.rhd')]
+    print('Accessing... ' + cell_path)
+    os.chdir(cell_path)
+    rhd_files = [file for file in os.listdir(cell_path) if file.endswith('.rhd')]
 
     for rhd in rhd_files:
-        a =  read_data(rhd)  # load the .rhd file
+        intan = read_data(rhd)  # load the .rhd file
+        # print(intan.keys())
+
+        plt.figure(figsize=(6, 4))
+        for ch in intan['amplifier_data']:
+            plt.plot(intan['t_amplifier'], ch, 'k')
     break
-
-
-
-
-
+    plt.show()
+    # break
