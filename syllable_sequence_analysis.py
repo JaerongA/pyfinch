ece1bf300ffbec4bcf70ee1394f6bf2a9ea6360f
JaerongAhn <jaerongahn@gmail.com>
2020-07-16 23:46:07 -0400

    function to analyze syllable sequence entropy

--- syllable_sequence_analysis.py
+++ syllable_sequence_analysis.py
@@ -1,7 +1,7 @@
 import os
 from summary import load
 from song_analysis.parameters import *
-
+import scipy.io
 
 summary_df, nb_cluster = load.summary(load.config())
 
@@ -9,6 +9,24 @@ for cluster_run in range(0, nb_cluster):
 
     cluster = load.cluster(summary_df, cluster_run)
     session_id, cell_id, session_path, cell_path = load.cluster_info(cluster)
-    print(session_path)
-    print(bout_crit)
+    print('Accessing... ' + cell_path)
+    os.chdir(cell_path)
+
+    mat_file = [file for file in os.listdir(cell_path) if file.endswith('.not.mat')]
+
+    for file in mat_file:
+        syllables = scipy.io.loadmat(file)['syllables']  # Load the syllable info
+
+
+
+
+
+
+        print(syllables)
+
+
+    # mat_file = scipy.io.loadmat(mat_file)['mat_file']
+    # print(mat_file)
+
+
     break
