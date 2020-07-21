8714278be7ea9d0f69940d5839c5bb1aaa8430bb
JaerongAhn <jaerongahn@gmail.com>
2020-07-20 23:50:22 -0400

    .

--- song_analysis/parameters.py
+++ song_analysis/parameters.py
@@ -1,4 +1,6 @@
+""" Key parameters for analyzing behavior (Song)"""
 
 bout_crit = 500  # the temporal criterion for separating song bouts (in ms)
-
+sample_rate = {'intan': 30000, 'recorder': 44000}  # sampling rate for audio signal (Hz)
+freq_range = [300, 8000]  # frequency range for bandpass filter for spectrogram (Hz)
 
