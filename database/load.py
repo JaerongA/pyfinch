3d95cc08398476b542f37ccab0b410357640dfa8
JaerongAhn <jaerongahn@gmail.com>
2020-08-04 23:57:59 -0400

    changed the database loading scheme

--- database/load.py
+++ database/load.py
@@ -3,15 +3,57 @@ import pandas as pd
 
 
 def database(*query):
+    # Apply query to the database
+    # Return cursor
     conn = sqlite3.connect('database/deafening.db')
-    # conn = sqlite3.connect('deafening.db')
     conn.row_factory = sqlite3.Row
     cur = conn.cursor()
 
     if query:
-        # cur = conn.execute('''SELECT * FROM cluster''')
         cur = conn.execute(query[0])
-    return conn, cur
+    return cur, conn
+
+
+def load_cluster(cluster_run):
+    conn = sqlite3.connect('database/deafening.db')
+    cur.execute("SELECT * FROM cluster WHERE id =?", str(cluster_run))
+    df = pd.read_sql_query("SELECT * FROM cluster WHERE id = (?)", conn, params=(cluster_run,))
+    for cluster in cur:
+        if len(cluster['id']) == 1:
+            cluster['id'] = '00' + cluster['id']
+        elif len(cluster['id']) == 2:
+            cluster['id'] = '0' + cluster['id']
+
+        #if len(cluster.TaskSession) == 1:
+        #    cluster.TaskSession = 'D0' + cluster.TaskSession
+        #elif len(cluster.TaskSession) == 2:
+        #    cluster.TaskSession = 'D' + cluster.TaskSession
+
+        #cluster.Site = cluster.Site[-2:]
+    return cur
+
+
+def load_song(conn, cluster_run):
+    cur = conn.execute('''SELECT * FROM song''')
+
+    if len(cluster['id']) == 1:
+        cluster['id'] = '00' + cluster['id']
+    elif len(cluster['id']) == 2:
+        cluster['id'] = '0' + cluster['id']
+
+    if len(cluster.TaskSession) == 1:
+        cluster.TaskSession = 'D0' + cluster.TaskSession
+    elif len(cluster.TaskSession) == 2:
+        cluster.TaskSession = 'D' + cluster.TaskSession
+    cluster.Site = cluster.Site[-2:]
+    return cluster
+
+
+
+
+
+
+
 
 
 # for row in rows:
@@ -33,4 +75,7 @@ def database(*query):
 # for row in rows:
 #    print(row)
 
-cursor = database()
+
+if __name__ == '__main__':
+    cur = database()
+
