1a2783da0e546df793c2a14ba6ad605be1a5f5dc
Jaerong Ahn <jaerongahn@gmail.com>
2020-09-19 23:55:05 -0400

    snr analysis

--- snr_analysis.py
+++ snr_analysis.py
@@ -7,14 +7,15 @@ from database import load
 from load_intan_rhd_format.load_intan_rhd_format import read_data
 
 
-query = "SELECT * FROM cluster WHERE birdID = 'g35r38'"
 
+query = "SELECT * FROM cluster WHERE id = '22'"
 cur, conn, col_names = load.database(query)
 
 for cell_info in cur.fetchall():
 
     cell_name, cell_path = load.cell_info(cell_info)
     # print(cell_name)
-    print(cell_path)
+    print('Loading... ' + cell_name)
 
 
+    # for file in [x for x in cell_path.iterdir() if x.is_dir()]:  # loop through the sub-dir
