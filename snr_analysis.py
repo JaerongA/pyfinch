fd740fa85a33f3609eab4f9b2ed2836580cf7f17
Jaerong Ahn <jaerongahn@gmail.com>
2020-09-21 23:58:37 -0400

    snr calculation

--- snr_analysis.py
+++ snr_analysis.py
@@ -5,17 +5,18 @@ Calculates a spike signal-to-noise ratio (SNR) relative to the background
 
 from database import load
 from load_intan_rhd_format.load_intan_rhd_format import read_data
-
+import numpy as np
+import scipy.io
+import matplotlib.pyplot as plt
 
 
 query = "SELECT * FROM cluster WHERE id = '22'"
 cur, conn, col_names = load.database(query)
 
 for cell_info in cur.fetchall():
-
     cell_name, cell_path = load.cell_info(cell_info)
-    # print(cell_name)
     print('Loading... ' + cell_name)
-
-
-    # for file in [x for x in cell_path.iterdir() if x.is_dir()]:  # loop through the sub-dir
+    mat_file = list(cell_path.glob('*' + cell_info['channel'] + '(merged).mat'))
+    channel_info = scipy.io.loadmat(mat_file[0])
+    print(channel_info)
+    
