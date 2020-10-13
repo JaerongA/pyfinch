e660e2fe09063990462ceb0db39feec0ee2a30a2
Jaerong Ahn <jaerongahn@gmail.com>
2020-10-12 23:31:56 -0400

    done

--- convert_adbit2volts.py
+++ convert_adbit2volts.py
@@ -5,15 +5,11 @@ It significantly decreased the amplitude of the waveform of those clusters isola
 This program converts the amplitude of those clusters by using ADbit value
 """
 
-from database import load
-import numpy as np
-from pathlib import Path
 
-
-def convert_adbit_volts(spk_waveform):
+def convert_adbit2volts(spk_waveform):
     """Input the waveform matrix extracted from the cluster .txt output"""
 
-    '''Parameters on the Offline Sorter'''
+    # Parameters on the Offline Sorter'''
     volt_range = 10  # in milivolts +- 5mV
     sampling_bits = 16
     volt_resolution = 0.000153  # volt_range/(2**sampling_bits)
@@ -21,30 +17,44 @@ def convert_adbit_volts(spk_waveform):
     return spk_waveform_new
 
 
-query = "SELECT * FROM cluster WHERE adbit_cluster IS TRUE"
-cur, conn, col_names = load.database(query)
+def converter(query):
+    """
+    Input: SQL query
+    Batch process from the database"""
+    from database import load
+    import numpy as np
+    from pathlib import Path
 
-for row in cur.fetchall():
-    cell_name, cell_path = load.cell_info(row)
-    print('Loading... ' + cell_name)
-    unit_nb = int(row['unit'][-2:])
+    cur, conn, col_names = load.database(query)
 
-    # Read from the cluster .txt file
-    spk_txt_file = list(cell_path.glob('*' + row['channel'] + '(merged).txt'))[0]
+    for row in cur.fetchall():
+        cell_name, cell_path = load.cell_info(row)
+        print('Loading... ' + cell_name)
+        unit_nb = int(row['unit'][-2:])
 
-    # Get the header
-    f = open(spk_txt_file, 'r')
-    header = f.readline()[:-1]
+        # Read from the cluster .txt file
+        spk_txt_file = list(cell_path.glob('*' + row['channel'] + '(merged).txt'))[0]
 
-    spk_info = np.loadtxt(spk_txt_file, delimiter='\t', skiprows=1)  # skip header
-    spk_waveform = spk_info[:, 3:]  # spike waveform
+        # Get the header
+        f = open(spk_txt_file, 'r')
+        header = f.readline()[:-1]
 
-    # Convert the value
-    spk_waveform_new = convert_adbit_volts(spk_waveform)
-    spk_txt_file_new = Path(spk_txt_file.parent, f"{spk_txt_file.stem}_new{spk_txt_file.suffix}")
+        spk_info = np.loadtxt(spk_txt_file, delimiter='\t', skiprows=1)  # skip header
+        spk_waveform = spk_info[:, 3:]  # spike waveform
 
-    # Replace the waveform  with new values
-    spk_info[:, 3:] = spk_waveform_new
+        # Convert the value
+        spk_waveform_new = convert_adbit2volts(spk_waveform)
+        spk_txt_file_new = Path(spk_txt_file.parent, f"{spk_txt_file.stem}_new{spk_txt_file.suffix}")
 
-    # Save to a new cluster .txt file
-    np.savetxt(spk_txt_file_new, spk_info, delimiter='\t', header=header, comments='', fmt='%f')
+        # Replace the waveform  with new values
+        spk_info[:, 3:] = spk_waveform_new
+
+        # Save to a new cluster .txt file
+        np.savetxt(spk_txt_file_new, spk_info, delimiter='\t', header=header, comments='', fmt='%f')
+        break
+
+
+if __name__ == '__main__':
+
+    query = "SELECT * FROM cluster WHERE adbit_cluster IS TRUE"
+    converter(query)
