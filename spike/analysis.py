3f2c02e45f8a3532e2a1dc68b5d8b828b0c0505c
Jaerong Ahn <jaerongahn@gmail.com>
2020-11-25 19:10:05 -0500

    update

--- spike/analysis.py
+++ spike/analysis.py
@@ -17,6 +17,8 @@ def get_event_info(cell_path):
     """
     Obtain event info & serialized timestamps for song & neural analysis
     """
+    ## Todo : change .wav timestamps instead of intan
+
     import numpy as np
 
     # List .rhd files
@@ -206,7 +208,6 @@ class ClusterInfo:
                 t_amplifier_serialized = np.append(t_amplifier_serialized, intan['t_amplifier'])
 
 
-
     def load_events(self):
         """
         Obtain event info & serialized timestamps for song & neural analysis
@@ -217,69 +218,9 @@ class ClusterInfo:
             event_dic = np.load(file_name, allow_pickle=True).item()
 
         else:
-            print('Creating events')
-            # List .rhd files
-            rhd_files = list(self.path.glob('*.rhd'))
+            event_dic = get_event_info(self.path)
 
-            # Initialize variables
-            t_amplifier_serialized = np.array([], dtype=np.float64)
-
-            # Store values in these lists
-            file_list = []
-            file_start_list = []
-            file_end_list = []
-            onset_list = []
-            offset_list = []
-            syllable_list = []
-            context_list = []
-
-            # Loop through Intan .rhd files
-            for file in rhd_files:
-
-                # Load the .rhd file
-                print('Loading... ' + file.stem)
-                intan = read_rhd(file)  # note that the timestamp is in second
-
-                # Load the .not.mat file
-                notmat_file = file.with_suffix('.wav.not.mat')
-                onsets, offsets, intervals, duration, syllables, context = read_not_mat(notmat_file, unit='ms')
-
-                # Serialize time stamps
-                intan['t_amplifier'] -= intan['t_amplifier'][0]  # start from t = 0
-                start_ind = t_amplifier_serialized.size  # start of the file
-
-                if t_amplifier_serialized.size == 0:
-                    t_amplifier_serialized = np.append(t_amplifier_serialized, intan['t_amplifier'])
-                else:
-                    intan['t_amplifier'] += (t_amplifier_serialized[-1] + (1 / sample_rate[self.format]))
-                    t_amplifier_serialized = np.append(t_amplifier_serialized, intan['t_amplifier'])
-
-                # File information (name, start and end timestamp of each file)
-                file_list.append(file.stem)
-                file_start_list.append(t_amplifier_serialized[start_ind] * 1E3)  # in ms
-                file_end_list.append(t_amplifier_serialized[-1] * 1E3)
-
-                onsets += intan['t_amplifier'][0] * 1E3  # convert to ms
-                offsets += intan['t_amplifier'][0] * 1E3
-
-                # Demarcate song bouts
-                onset_list.append(demarcate_bout(onsets, intervals))
-                offset_list.append(demarcate_bout(offsets, intervals))
-                syllable_list.append(demarcate_bout(syllables, intervals))
-                context_list.append(context)
-
-            # Organize event-related info into a single dictionary object
-            event_dic = {
-                'files': file_list,
-                'file_start': file_start_list,
-                'file_end': file_end_list,
-                'onsets': onset_list,
-                'offsets': offset_list,
-                'syllables': syllable_list,
-                'context': context_list
-            }
-
-            # Save as a numpy object
+            # Save event_dic as a numpy object
             np.save(file_name, event_dic)
 
         # Set the dictionary values to class attributes
@@ -302,6 +243,29 @@ class ClusterInfo:
         nb_files['ALL'] = len([context for context in self.context if context == 'U'])
         return nb_files
 
+    def get_nb_bouts(self):
+        pass
+
+    def get_nb_motifs(self):
+        pass
+
+    def get_peth(self):
+        """Get peri-event time histograms & rasters"""
+        pass
+
+
 class RawData(ClusterInfo):
-    # def __init__(self, database):
-    pass
+    def __init__(self, database):
+        super().__init__(database)
+
+    def load_audio(self):
+        pass
+
+    def load_neural_trace(self, channel):
+        pass
+
+    def load_timestamp(self):
+        pass
+
+
+
