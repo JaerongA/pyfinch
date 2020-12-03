eb69e476e94a1f4d84981e52b473cf4b5320374c
Jaerong Ahn <jaerongahn@gmail.com>
2020-12-02 22:47:56 -0500

    added param for correlogram

--- spike/parameters.py
+++ spike/parameters.py
@@ -8,3 +8,6 @@ sample_rate = {'rhd': 30000, 'cbin': 32000, 'recorder': 44000}  # sampling rate 
 # Define baseline period 1 sec window & 2 sec prior to syllable onset
 baseline = {'time_win': 1000, 'time_buffer': 2000}  # in ms
 peth_buffer = 500 # in ms
+
+# For spike correlogram
+corr = {'bin_size': 1, 'lag': 100}  # in ms
