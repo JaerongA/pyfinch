9ec4b696c62a9159b974b12a8c2598e6e0010ce1
Jaerong Ahn <jaerongahn@gmail.com>
2020-12-09 00:10:39 -0500

    include time bin parameter in spk_corr_parm
    Signed-off-by: Jaerong Ahn <jaerongahn@gmail.com>

--- spike/parameters.py
+++ spike/parameters.py
@@ -2,6 +2,8 @@
 By Jaerong
 Key parameters for analyzing neural data
 """
+import numpy as np
+
 
 sample_rate = {'rhd': 30000, 'cbin': 32000, 'recorder': 44000}  # sampling rate for audio signal (Hz)
 
@@ -10,6 +12,7 @@ baseline = {'time_win': 1000, 'time_buffer': 2000}  # in ms
 
 # For spike correlogram
 spk_corr_parm = {'bin_size': 1, 'lag': 100}  # in ms
+spk_corr_parm['time_bin'] = np.arange(-spk_corr_parm['lag'], spk_corr_parm['lag'] + 1, spk_corr_parm['bin_size'])
 
 # For peth (peri-event time histogram) or rasters
 peth = {'buffer': 500}  # time buffer before the event onset in ms
