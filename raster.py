f2beed6608915fb2f905a56b01d028080a8ad5aa
Jaerong Ahn <jaerongahn@gmail.com>
2020-12-15 00:26:13 -0500

    revision with gridspec
    Signed-off-by: Jaerong Ahn <jaerongahn@gmail.com>

--- raster.py
+++ raster.py
@@ -9,6 +9,7 @@ from pathlib import Path
 from spike.load import read_rhd
 import matplotlib.pyplot as plt
 import matplotlib.colors as colors
+import matplotlib.gridspec as gridspec
 from util import save
 from util.spect import *
 from util.draw import *
@@ -20,11 +21,10 @@ project = ProjectLoader()
 cur, conn, col_names = project.load_db(query)
 
 
-
-rec_height = 0.1  # syllable duration rect
-text_yloc = rec_height + 0.1  # text height
-nb_row = 2
-nb_cols = 1
+rec_yloc = 0.05
+rec_height = 0.3  # syllable duration rect
+text_yloc = rec_height + 0.2 # text height
+font_size = 10
 
 for row in cur.fetchall():
 
@@ -33,8 +33,6 @@ for row in cur.fetchall():
     mi = MotifInfo(row)
 
     # Plot spectrogram & peri-event histogram
-
-    # for onset, offset in zip(mi.onsets, mi.offsets):
     for onset, offset in zip(mi.onsets, mi.offsets):
 
         # Convert from string to array of floats
@@ -49,26 +47,13 @@ for row in cur.fetchall():
         audio = AudioData(row).extract([start, end])
         audio.spectrogram(freq_range=freq_range)
 
-        fig, (ax_syl, ax_spect) = plt.subplots(nb_row, nb_cols, figsize=(7, 3), sharex=True)
-
-        # Plot syllable duration
-        syl_dur = offset - onset  # syllable duration
-        onset -= onset[0] # start from 0
-        offset = onset + syl_dur
-
-        for i, syl in enumerate(mi.motif):
-
-            # Mark syllables
-            rectangle = plt.Rectangle((onset[i], 0.01), syl_dur[i], rec_height,
-                                      linewidth=1, edgecolor='k', facecolor=syl_color['Motif'][i])
-            ax_syl.add_patch(rectangle)
-            ax_syl.text((onset[i] + (offset[i] - onset[i]) / 2), text_yloc, syl)
-
-        ax_syl.axis('off')
-        ax_syl.text((onset[i] + (offset[i] - onset[i]) / 2), text_yloc, syl)
-        # plt.title(mi.name)
+        # Plot figure
+        fig = plt.figure(figsize=(7,3.5))
+        plt.suptitle(mi.name, y=.95)
+        gs = gridspec.GridSpec(7, 1)
 
         # Plot spectrogram
+        ax_spect = plt.subplot(gs[1:-1])
         ax_spect.pcolormesh(audio.timebins*1E3 - peth['buffer'], audio.freqbins, audio.spect,  # data
                             cmap='hot_r',
                             norm=colors.SymLogNorm(linthresh=0.05,
@@ -77,14 +62,24 @@ for row in cur.fetchall():
                                                    vmax=100
                                                    ))
 
-
         remove_right_top(ax_spect)
-        ax_spect.set_ylim(300, 8000)
-        plt.subplots_adjust(top=1, bottom=0, right=1, left=0,
-                            hspace=0, wspace=0)
-        # plt.margins(0, 0)
-        # plt.margins(x=0, y=0.001)
-        # fig.tight_layout()
-        fig.suptitle(mi.name)
+        ax_spect.set_ylim(freq_range[0], freq_range[1])
+        ax_spect.set_ylabel('Frequency (Hz)', fontsize=font_size)
+        ax_spect.set_xlabel('Time (ms)', fontsize=font_size)
+
+        # Plot syllable duration
+        ax_syl = plt.subplot(gs[0], sharex= ax_spect)
+        syl_dur = offset - onset  # syllable duration
+        onset -= onset[0]  # start from 0
+        offset = onset + syl_dur
+
+        for i, syl in enumerate(mi.motif):
+            # Mark syllables
+            rectangle = plt.Rectangle((onset[i], rec_yloc), syl_dur[i], rec_height,
+                                      linewidth=1, edgecolor='k', facecolor=syl_color['Motif'][i])
+            ax_syl.add_patch(rectangle)
+            ax_syl.text((onset[i] + (offset[i] - onset[i]) / 2), text_yloc, syl, size=font_size)
+        ax_syl.axis('off')
+
         plt.show()
         break
