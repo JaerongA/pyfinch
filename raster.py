0a417e7f703fdd8266e727a879feb0e4f6383e91
Jaerong Ahn <jaerongahn@gmail.com>
2021-01-11 22:48:21 -0500

    added text outputs
    Signed-off-by: Jaerong Ahn <jaerongahn@gmail.com>

--- raster.py
+++ raster.py
@@ -1,4 +1,7 @@
-# Practice creating an audio object
+"""
+By Jaerong
+plot raster & peth
+"""
 
 from database.load import ProjectLoader
 from analysis.spike import *
@@ -13,7 +16,6 @@ from util.spect import *
 from util.draw import *
 from scipy.ndimage import gaussian_filter1d
 
-
 # Select statement
 query = "SELECT * FROM cluster WHERE id == 96"
 
@@ -25,12 +27,12 @@ rec_yloc = 0.05
 rec_height = 1  # syllable duration rect
 text_yloc = 0.5  # text height
 font_size = 10
-update=False
+update = False
 
 # Loop through neurons
 for row in cur.fetchall():
 
-    # ci = ClusterInfo(row, update=False)
+    # ci = ClusterInfo(row, update=True)
     mi = MotifInfo(row, update=True)
 
     # Plot spectrogram & peri-event histogram (Just the first rendition)
@@ -52,13 +54,13 @@ for row in cur.fetchall():
     audio.spectrogram(freq_range=freq_range)
 
     # Plot figure
-    fig = plt.figure(figsize=(5.5,9))
+    fig = plt.figure(figsize=(8, 9))
     plt.suptitle(mi.name, y=.95)
-    gs = gridspec.GridSpec(18, 1)
+    gs = gridspec.GridSpec(18, 6)
     gs.update(wspace=0.025, hspace=0.05)
 
     # Plot spectrogram
-    ax_spect = plt.subplot(gs[1:3])
+    ax_spect = plt.subplot(gs[1:3, 0:4])
     ax_spect.pcolormesh(audio.timebins * 1E3 - peth_parm['buffer'], audio.freqbins, audio.spect,  # data
                         cmap='hot_r',
                         norm=colors.SymLogNorm(linthresh=0.05,
@@ -75,7 +77,7 @@ for row in cur.fetchall():
     # ax_spect.set_xlabel('Time (ms)', fontsize=font_size)
 
     # Plot syllable duration
-    ax_syl = plt.subplot(gs[0], sharex= ax_spect)
+    ax_syl = plt.subplot(gs[0, 0:4], sharex=ax_spect)
     note_dur = offset - onset  # syllable duration
     onset -= onset[0]  # start from 0
     offset = onset + note_dur
@@ -83,16 +85,15 @@ for row in cur.fetchall():
     # Mark syllables
     for i, syl in enumerate(mi.motif):
         rectangle = plt.Rectangle((onset[i], rec_yloc), note_dur[i], 0.2,
-                                  linewidth=1, alpha = 0.5, edgecolor='k', facecolor=note_color['Motif'][i])
+                                  linewidth=1, alpha=0.5, edgecolor='k', facecolor=note_color['Motif'][i])
         ax_syl.add_patch(rectangle)
         ax_syl.text((onset[i] + (offset[i] - onset[i]) / 2), text_yloc, syl, size=font_size)
     ax_syl.axis('off')
 
-
     # Plot raster
-    line_offsets = np.arange(0.5,len(mi))
+    line_offsets = np.arange(0.5, len(mi))
     zipped_lists = zip(mi.contexts, mi.spk_ts_warp, mi.onsets)
-    ax_raster = plt.subplot(gs[4:6], sharex= ax_spect)
+    ax_raster = plt.subplot(gs[4:6, 0:4], sharex=ax_spect)
 
     pre_context = ''  # for marking  context change
     context_change = np.array([])
@@ -105,7 +106,7 @@ for row in cur.fetchall():
         # print("spk ={}, nb = {}".format(spk, len(spk)))
         # print('')
         ax_raster.eventplot(spk, colors='k', lineoffsets=line_offsets[motif_ind],
-                     linelengths=1, orientation='horizontal')
+                            linelengths=1, orientation='horizontal')
 
         # Demarcate the note
         k = 1  # index for setting the motif color
@@ -118,7 +119,7 @@ for row in cur.fetchall():
                                           linewidth=1,
                                           alpha=0.15,
                                           facecolor=note_color['Motif'][i])
-            elif not i%2:
+            elif not i % 2:
                 # print("i is {}, color is {}".format(i, i-k))
                 rectangle = plt.Rectangle((sum(mi.median_durations[:i]), motif_ind), mi.median_durations[i], rec_height,
                                           fill=True,
@@ -131,7 +132,7 @@ for row in cur.fetchall():
         # Demarcate song block (undir vs dir) with a horizontal line
         if pre_context != context:
 
-            ax_raster.axhline(y=motif_ind-1, color='k', ls='-', lw=0.5)
+            ax_raster.axhline(y=motif_ind - 1, color='k', ls='-', lw=0.5)
             context_change = np.append(context_change, (motif_ind))
             if pre_context:
                 ax_raster.text(ax_raster.get_xlim()[1] + 0.2,
@@ -146,17 +147,16 @@ for row in cur.fetchall():
                    pre_context,
                    size=6)
 
-    ax_raster.set_ylim(0,len(mi))
+    ax_raster.set_ylim(0, len(mi))
     ax_raster.set_ylabel('Trial #', fontsize=font_size)
     # ax_raster.set_xlabel('Time (ms)', fontsize=font_size)
     plt.yticks([0, len(mi)], [str(0), str(len(mi))])
     remove_right_top(ax_raster)
     ax_raster.set_xticks([])  # remove xticks
 
-
     # Plot sorted raster
-    ax_raster = plt.subplot(gs[7:9], sharex= ax_spect)
-    line_offsets = np.arange(0.5,len(mi))
+    ax_raster = plt.subplot(gs[7:9, 0:4], sharex=ax_spect)
+    line_offsets = np.arange(0.5, len(mi))
 
     # Sort trials based on context
     sort_ind = np.array([i[0] for i in sorted(enumerate(mi.contexts), key=lambda x: x[1], reverse=True)])
@@ -177,7 +177,7 @@ for row in cur.fetchall():
         # print("spk ={}, nb = {}".format(spk, len(spk)))
         # print('')
         ax_raster.eventplot(spk, colors='k', lineoffsets=line_offsets[motif_ind],
-                     linelengths=1, orientation='horizontal')
+                            linelengths=1, orientation='horizontal')
 
         # Demarcate the note
         k = 1  # index for setting the motif color
@@ -190,7 +190,7 @@ for row in cur.fetchall():
                                           linewidth=1,
                                           alpha=0.15,
                                           facecolor=note_color['Motif'][i])
-            elif not i%2:
+            elif not i % 2:
                 # print("i is {}, color is {}".format(i, i-k))
                 rectangle = plt.Rectangle((sum(mi.median_durations[:i]), motif_ind), mi.median_durations[i], rec_height,
                                           fill=True,
@@ -203,7 +203,7 @@ for row in cur.fetchall():
         # Demarcate song block (undir vs dir) with a horizontal line
         if pre_context != context:
 
-            ax_raster.axhline(y=motif_ind-1, color='k', ls='-', lw=0.5)
+            ax_raster.axhline(y=motif_ind - 1, color='k', ls='-', lw=0.5)
             context_change = np.append(context_change, (motif_ind))
             if pre_context:
                 ax_raster.text(ax_raster.get_xlim()[1] + 0.2,
@@ -218,7 +218,7 @@ for row in cur.fetchall():
                    pre_context,
                    size=6)
 
-    ax_raster.set_ylim(0,len(mi))
+    ax_raster.set_ylim(0, len(mi))
     ax_raster.set_ylabel('Trial #', fontsize=font_size)
     # ax_raster.set_xlabel('Time (ms)', fontsize=font_size)
     ax_raster.set_title('sorted raster', size=font_size)
@@ -227,7 +227,7 @@ for row in cur.fetchall():
     remove_right_top(ax_raster)
 
     # Draw peri-event histogram (PETH)
-    ax_peth = plt.subplot(gs[10:12], sharex= ax_spect)
+    ax_peth = plt.subplot(gs[10:12, 0:4], sharex=ax_spect)
 
     # Get peth object
     pi = mi.get_peth()  # peth info
@@ -254,5 +254,45 @@ for row in cur.fetchall():
     ax_peth.axvline(x=mi.median_durations.sum(), color='k', ls='--', lw=0.5)
     remove_right_top(ax_peth)
 
+    # Calculate pairwise cross-correlation
+    pi.get_pcc()
 
-    plt.show()
+    # Print out results on the figure
+    txt_xloc = -0.5
+    txt_yloc = 1
+    txt_inc = 0.05  # y-distance between texts within the same section
+    txt_offset = 0.1
+
+    ax_text = plt.subplot(gs[::, 5])
+    ax_text.set_axis_off()  # remove all axes
+
+    # # of motifs
+    nb_motifs = mi.nb_motifs
+    nb_motifs.pop('All', None)
+    for i, (k, v) in enumerate(nb_motifs.items()):
+        txt_yloc -= txt_inc
+        ax_text.text(txt_xloc, txt_yloc, f"# of motifs ({k}) = {v}", fontsize=font_size)
+
+    # PCC
+    txt_yloc -= txt_offset
+    v = pi.pcc['U']['mean'] if "U" in pi.pcc else np.nan
+    ax_text.text(txt_xloc, txt_yloc, f"PCC (U) = {v}", fontsize=font_size)
+    txt_yloc -= txt_inc
+
+    v = pi.pcc['D']['mean'] if "D" in pi.pcc else np.nan
+    ax_text.text(txt_xloc, txt_yloc, f"PCC (D) = {v}", fontsize=font_size)
+
+    # Corr context (correlation of firing rates between two contexts)
+    txt_yloc -= txt_offset
+    print(txt_yloc)
+    corr_context = None
+    try:
+        corr_context = round(np.corrcoef(pi.mean_fr['U'], pi.mean_fr['D'])[0, 1], 3)
+    finally:
+        ax_text.text(txt_xloc, txt_yloc, f"Context Corr = {corr_context}", fontsize=font_size)
+
+    # Save results
+    save_path = save.make_dir(project.path / 'Analysis', 'Spk')
+    save.save_fig(fig, save_path, mi.name, ext='.png')
+
+    # plt.show()
