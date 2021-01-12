7c3863461691b8a92d817ff37e6ffc248e0ae02f
Jaerong Ahn <jaerongahn@gmail.com>
2021-01-11 22:47:10 -0500

    revision to functions
    Signed-off-by: Jaerong Ahn <jaerongahn@gmail.com>

--- util/save.py
+++ util/save.py
@@ -8,17 +8,23 @@ from datetime import date
 from database import load
 
 
-def make_dir(parent_path, dir_name: str, add_date=True):
+def make_dir(parent_path, dir_name, add_date=True):
+    """
+    add date info in the sub-directory
 
-    # # add date info in the sub-directory
-    # project_path = load.project()
-    # save_path = project_path / 'Analysis' / dir_name
+    Args:
+        parent_dir: path
+        dir_name: str
+        add_date: bool
 
-    save_path = Path(parent_path) /  dir_name
+    Returns:
+        save_path: path
+    """
 
+    global save_path
     if add_date:
         today = date.today()
-        save_path = save_path / today.strftime("%Y-%m-%d")  # 2020-07-04
+        save_path = parent_path / dir_name / today.strftime("%Y-%m-%d")  # 2020-07-04
 
     # print(save_path)
     if not save_path.exists():
@@ -26,10 +32,10 @@ def make_dir(parent_path, dir_name: str, add_date=True):
     return save_path
 
 
-def save_fig(fig, save_path, title, ext='.png'):
-
+def save_fig(fig, save_path, title, ext='.png', open_folder=True):
     import matplotlib.pyplot as plt
     import matplotlib
+    from util.functions import open_folder
 
     # Make the text in .pdf editable
     # pdf.fonttype : 42 # Output Type 3 (Type3) or Type 42 (TrueType)
@@ -44,8 +50,11 @@ def save_fig(fig, save_path, title, ext='.png'):
     plt.savefig(fig_name, transparent=True)
     plt.close(fig)
 
-def save2json(filename, data):
+    if open_folder:  # open folder after saving figures
+        open_folder(save_path)
 
+
+def save2json(filename, data):
     # save the song bout & number of bouts in .json
     import json
     with open(filename, 'w') as f:
