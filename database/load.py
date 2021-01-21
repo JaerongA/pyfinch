8191d3e9a1b40d3806b15ce57d98f7af691fd80e
Jaerong Ahn <jaerongahn@gmail.com>
2021-01-21 02:12:45 -0500

    created DBLoader
    Signed-off-by: Jaerong Ahn <jaerongahn@gmail.com>

--- database/load.py
+++ database/load.py
@@ -16,9 +16,6 @@ class ProjectLoader:
         self.db_path = Path(config.get('path', 'database'))
         self.db = Path(config.get('file', 'database'))
         self.parameter = config['parameter']
-        # self.parameter = config.get('parameter')
-        # print(config.sections())
-        # print(config['path'])
 
     @property
     def open_folder(self):
@@ -26,65 +23,22 @@ class ProjectLoader:
         import webbrowser
         webbrowser.open(self.path)
 
-    def load_db(self, query):
-        database_path = self.path / self.db_path / self.db
-        conn = sqlite3.connect(database_path)
-        conn.row_factory = sqlite3.Row
-        cur = conn.cursor()
+    def load_db(self):
+        db_path = self.path / self.db_path / self.db
+        return DBLoader(db_path)
+
+
+class DBLoader:
+    def __init__(self, db_path):
+
+        self.conn = sqlite3.connect(db_path)
+        self.conn.row_factory = sqlite3.Row
+        self.cur = self.conn.cursor()
+
+    def execute(self, query):
         # get column names
-        cur.execute(query)
-        row = cur.fetchone()
-        col_names = row.keys()
-        cur.execute(query)
+        self.cur.execute(query)
+        self.row = self.cur.fetchone()
+        self.col_names = self.row.keys()
+        self.cur.execute(query)
 
-        return cur, conn, col_names
-
-    def load_parms(self):
-        pass
-
-# def cluster_info(row):
-#     project_path = project()
-#
-#     if len(str(row['id'])) == 1:
-#         cluster_id = '00' + str(row['id'])
-#     elif len(str(row['id'])) == 2:
-#         cluster_id = '0' + str(row['id'])
-#
-#     if len(str(row['taskSession'])) == 1:
-#         cluster_taskSession = 'D0' + str(str(row['taskSession']))
-#     elif len(str(row['taskSession'])) == 2:
-#         cluster_taskSession = 'D' + str(str(row['taskSession']))
-#
-#     cluster_name = [cluster_id, row['birdID'], row['taskName'], cluster_taskSession, row['sessionDate'], row['site'],
-#                  row['channel'], row['unit']]
-#     cluster_name = '-'.join(map(str, cluster_name))
-#
-#     cluster_path = os.path.join(project_path, row['birdID'], row['taskName'],
-#                              cluster_taskSession + '(' + str(row['sessionDate']) + ')', row['site'][-2:],
-#                              'Songs')
-#     cluster_path = Path(cluster_path)
-#
-#     return cluster_name, cluster_path
-#
-#
-# def song_info(row):
-#
-#     project_path = project()
-#
-#     if len(str(row['id'])) == 1:
-#         song_id = '00' + str(row['id'])
-#     elif len(str(row['id'])) == 2:
-#         song_id = '0' + str(row['id'])
-#
-#     if len(str(row['taskSession'])) == 1:
-#         song_taskSession = 'D0' + str(str(row['taskSession']))
-#     elif len(str(row['taskSession'])) == 2:
-#         song_taskSession = 'D' + str(str(row['taskSession']))
-#
-#     song_name = [song_id, row['birdID'], row['taskName'], song_taskSession, row['sessionDate']]
-#     song_name = '-'.join(map(str, song_name))
-#     song_path = os.path.join(project_path, row['birdID'], row['taskName'],
-#                              song_taskSession + '(' + str(row['sessionDate']) + ')')
-#     song_path = Path(song_path)
-#
-#     return song_name, song_path
