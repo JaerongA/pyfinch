d525ca837598c19fca3023368367cdd853f39b33
Jaerong Ahn <jaerongahn@gmail.com>
2021-02-23 23:32:05 -0500

    update
    Signed-off-by: Jaerong Ahn <jaerongahn@gmail.com>

--- plot_bout.py
+++ plot_bout.py
@@ -13,7 +13,14 @@ save_fig = True
 update = False
 dir_name = 'RasterBouts'
 fig_ext = '.png'  # .png or .pdf
-
+font_size = 12  # figure font size
+rec_yloc = 0.05
+rect_height = 0.4
+text_yloc = 1  # text height
+nb_row = 10
+nb_col = 1
+tick_length = 1
+tick_width = 1
 
 class BoutInfo(ClusterInfo):
     """Child class of ClusterInfo"""
@@ -43,6 +50,9 @@ class BoutInfo(ClusterInfo):
     def print_name(self):
         print('')
         print('Load bout {self.name}'.format(self=self))
+
+    def __len__(self):
+        return len(self.files)
 
     def load_bouts(self):
         # Store values here
@@ -119,32 +129,66 @@ for row in db.cur.fetchall():
     audio = AudioData(path, update=update)  # audio object
     bi = BoutInfo(path, channel_nb, unit_nb, cluster_db.songNote, format, name, update=update)  # bout object
 
+    list_zip = zip(bi.files, bi.spk_ts, bi.onsets, bi.offsets, bi.syllables, bi.contexts)
 
+    for bout_ind, (file, spks, onsets, offsets, syllables, context) in enumerate(list_zip):
 
-    # # Bout start and end
-    # start = onset[0] - peth_parm['buffer']
-    # end = offset[-1] + peth_parm['buffer']
-    # duration = offset[-1] - onset[0]
-    #
-    # # Get spectrogram
-    # audio = AudioData(row).extract([start, end])
-    # audio.spectrogram(freq_range=freq_range)
-    #
-    # # Plot figure
-    # fig = plt.figure(figsize=(8, 9), dpi=800)
-    # # Plot spectrogram
-    # ax_spect = plt.subplot(gs[1:3, 0:4])
-    # ax_spect.pcolormesh(audio.timebins * 1E3 - peth_parm['buffer'], audio.freqbins, audio.spect,  # data
-    #                     cmap='hot_r',
-    #                     norm=colors.SymLogNorm(linthresh=0.05,
-    #                                            linscale=0.03,
-    #                                            vmin=0.5,
-    #                                            vmax=100
-    #                                            ))
-    #
-    # remove_right_top(ax_spect)
-    # ax_spect.set_xlim(-peth_parm['buffer'], duration + peth_parm['buffer'])
-    # ax_spect.set_ylim(freq_range[0], freq_range[1])
-    # ax_spect.set_ylabel('Frequency (Hz)', fontsize=font_size)
-    # plt.yticks(freq_range, [str(freq_range[0]), str(freq_range[1])])
-    # plt.setp(ax_spect.get_xticklabels(), visible=False)
+        # Convert from string to array of floats
+        onsets = np.asarray(list(map(float, onsets)))
+        offsets = np.asarray(list(map(float, offsets)))
+
+        # bout start and end
+        start = onsets[0] - peth_parm['buffer']
+        end = offsets[-1] + peth_parm['buffer']
+        duration = offsets[-1] - onsets[0]
+
+        # Get spectrogram
+        audio.extract([start, end])
+        audio.spectrogram(freq_range=freq_range)
+
+        # Plot figure
+        fig = plt.figure(figsize=(10, 6))
+        fig_name = f"{file} - Bout # {bout_ind + 1}"
+        fig.suptitle(fig_name, y=0.98)
+
+        # Plot spectrogram
+        ax_spect = plt.subplot2grid((nb_row, nb_col), (2, 0), rowspan=2, colspan=1)
+        ax_spect.pcolormesh(audio.timebins * 1E3 - peth_parm['buffer'], audio.freqbins, audio.spect,  # data
+                            cmap='hot_r',
+                            norm=colors.SymLogNorm(linthresh=0.05,
+                                                   linscale=0.03,
+                                                   vmin=0.5,
+                                                   vmax=100
+                                                   ))
+
+        remove_right_top(ax_spect)
+        ax_spect.set_xlim(-peth_parm['buffer'], duration + peth_parm['buffer'])
+        ax_spect.set_ylim(freq_range[0], freq_range[1])
+        ax_spect.set_ylabel('Frequency (Hz)', fontsize=font_size)
+        plt.yticks(freq_range, [str(freq_range[0]), str(freq_range[1])])
+        plt.setp(ax_spect.get_xticklabels(), visible=False)
+
+        # Plot syllable duration
+        ax_syl = plt.subplot2grid((nb_row, nb_col), (1, 0), rowspan=1, colspan=1, sharex=ax_spect)
+        note_dur = offsets - onsets  # syllable duration
+        onsets -= onsets[0]  # start from 0
+        offsets = onsets + note_dur
+
+        # Mark syllables
+        for i, syl in enumerate(syllables):
+            rectangle = plt.Rectangle((onsets[i], rec_yloc), note_dur[i], rect_height,
+                                      linewidth=1, alpha=0.5, edgecolor='k', facecolor=bout_color[syl])
+            ax_syl.add_patch(rectangle)
+            ax_syl.text((onsets[i] + (offsets[i] - onsets[i]) / 2), text_yloc, syl, size=font_size)
+        ax_syl.axis('off')
+
+        # Plot rasters
+        ax_raster = plt.subplot2grid((nb_row, nb_col), (4, 0), rowspan=1, colspan=1, sharex=ax_spect)
+        spks = spks - float(onsets[0])
+        ax_raster.eventplot(spks, colors='k', lineoffsets=0.5,
+                            linelengths=tick_length, linewidths=tick_width, orientation='horizontal')
+        remove_right_top(ax_raster)
+
+        plt.show()
+
+        break
