232d11af52b74ed1cd0f5f90b8cb95bf98c18e68
Jaerong Ahn <jaerongahn@gmail.com>
2021-02-25 02:10:25 -0500

    plot song bout and spike
    Signed-off-by: Jaerong Ahn <jaerongahn@gmail.com>

--- plot_bout.py
+++ plot_bout.py
@@ -7,6 +7,7 @@ from util.draw import *
 import matplotlib.colors as colors
 import matplotlib.gridspec as gridspec
 from database.load import DBInfo
+from scipy import stats
 
 # Parameters
 save_fig = True
@@ -15,7 +16,7 @@ dir_name = 'RasterBouts'
 fig_ext = '.png'  # .png or .pdf
 font_size = 12  # figure font size
 rec_yloc = 0.05
-rect_height = 0.4
+rect_height = 0.3
 text_yloc = 1  # text height
 nb_row = 10
 nb_col = 1
@@ -125,8 +126,6 @@ for row in db.cur.fetchall():
     format = cluster_db.format
 
     ci = ClusterInfo(path, channel_nb, unit_nb, format, name, update=update)  # cluster object
-    nd = NeuralData(path, channel_nb, format, update=update)  # raw neural data
-    audio = AudioData(path, update=update)  # audio object
     bi = BoutInfo(path, channel_nb, unit_nb, cluster_db.songNote, format, name, update=update)  # bout object
 
     list_zip = zip(bi.files, bi.spk_ts, bi.onsets, bi.offsets, bi.syllables, bi.contexts)
@@ -136,6 +135,7 @@ for row in db.cur.fetchall():
         # Convert from string to array of floats
         onsets = np.asarray(list(map(float, onsets)))
         offsets = np.asarray(list(map(float, offsets)))
+        spks = spks - onsets[0]
 
         # bout start and end
         start = onsets[0] - peth_parm['buffer']
@@ -143,17 +143,19 @@ for row in db.cur.fetchall():
         duration = offsets[-1] - onsets[0]
 
         # Get spectrogram
-        audio.extract([start, end])
-        audio.spectrogram(freq_range=freq_range)
+        audio = AudioData(path, update=update).extract([start, end])  # audio object
+        audio.spectrogram()
+        audio.spect_time = audio.spect_time - audio.spect_time[0] - peth_parm['buffer']
 
         # Plot figure
-        fig = plt.figure(figsize=(10, 6))
+        fig = plt.figure(figsize=(8, 7))
+        fig.tight_layout()
         fig_name = f"{file} - Bout # {bout_ind + 1}"
-        fig.suptitle(fig_name, y=0.98)
+        fig.suptitle(fig_name, y=0.95)
 
         # Plot spectrogram
         ax_spect = plt.subplot2grid((nb_row, nb_col), (2, 0), rowspan=2, colspan=1)
-        ax_spect.pcolormesh(audio.timebins * 1E3 - peth_parm['buffer'], audio.freqbins, audio.spect,  # data
+        ax_spect.pcolormesh(audio.spect_time, audio.spect_freq, audio.spect,  # data
                             cmap='hot_r',
                             norm=colors.SymLogNorm(linthresh=0.05,
                                                    linscale=0.03,
@@ -162,11 +164,11 @@ for row in db.cur.fetchall():
                                                    ))
 
         remove_right_top(ax_spect)
-        ax_spect.set_xlim(-peth_parm['buffer'], duration + peth_parm['buffer'])
         ax_spect.set_ylim(freq_range[0], freq_range[1])
         ax_spect.set_ylabel('Frequency (Hz)', fontsize=font_size)
         plt.yticks(freq_range, [str(freq_range[0]), str(freq_range[1])])
         plt.setp(ax_spect.get_xticklabels(), visible=False)
+        plt.xlim([audio.spect_time[0]-100, audio.spect_time[-1]+100])
 
         # Plot syllable duration
         ax_syl = plt.subplot2grid((nb_row, nb_col), (1, 0), rowspan=1, colspan=1, sharex=ax_spect)
@@ -182,12 +184,38 @@ for row in db.cur.fetchall():
             ax_syl.text((onsets[i] + (offsets[i] - onsets[i]) / 2), text_yloc, syl, size=font_size)
         ax_syl.axis('off')
 
+        # Plot song amplitude
+        audio.data = stats.zscore(audio.data)
+        audio.timestamp = audio.timestamp - audio.timestamp[0] - peth_parm['buffer']
+        ax_amp = plt.subplot2grid((nb_row, nb_col), (4, 0), rowspan=2, colspan=1, sharex=ax_spect)
+        ax_amp.plot(audio.timestamp, audio.data, 'k')
+        ax_amp.axis('off')
+
         # Plot rasters
-        ax_raster = plt.subplot2grid((nb_row, nb_col), (4, 0), rowspan=1, colspan=1, sharex=ax_spect)
-        spks = spks - float(onsets[0])
+        ax_raster = plt.subplot2grid((nb_row, nb_col), (6, 0), rowspan=2, colspan=1, sharex=ax_spect)
+        # spks2 = spks - start -peth_parm['buffer'] -peth_parm['buffer']
         ax_raster.eventplot(spks, colors='k', lineoffsets=0.5,
                             linelengths=tick_length, linewidths=tick_width, orientation='horizontal')
-        remove_right_top(ax_raster)
+        ax_raster.axis('off')
+
+        # Plot raw neural data
+        nd = NeuralData(path, channel_nb, format, update=update).extract([start, end])  # raw neural data
+        nd.timestamp = nd.timestamp - nd.timestamp[0] - peth_parm['buffer']
+        ax_nd = plt.subplot2grid((nb_row, nb_col), (8, 0), rowspan=2, colspan=1, sharex=ax_spect)
+        ax_nd.plot(nd.timestamp, nd.data, 'k')
+
+        # Add a scale bar
+        plt.plot([ax_nd.get_xlim()[0] + 50, ax_nd.get_xlim()[0] + 50],
+                 [-250, 250], 'k', lw=3)  # for amplitude
+        plt.text(ax_nd.get_xlim()[0] , -200, '500 µV', rotation=90)
+        plt.subplots_adjust(wspace=0, hspace=0)
+        remove_right_top(ax_nd)
+        ax_nd.spines['left'].set_visible(False)
+        plt.yticks([], [])
+        ax_nd.set_xlabel('Time (ms)')
+
+
+
 
         plt.show()
 
