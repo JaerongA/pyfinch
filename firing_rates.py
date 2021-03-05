56d3b4bd88cb4865a91d099835151af13a8b9bd7
Jaerong Ahn <jaerongahn@gmail.com>
2021-03-05 15:15:25 -0500

    update with a new class object
    Signed-off-by: Jaerong Ahn <jaerongahn@gmail.com>

--- firing_rates.py
+++ firing_rates.py
@@ -7,25 +7,33 @@ Get mean firing rates per condition
 from contextlib import suppress
 
 from analysis.spike import *
-from database.load import ProjectLoader
+from database.load import ProjectLoader, DBInfo
 
 # Parameters
 update = True
-update_db = True
+update_db = False
 
 # Load database
 db = ProjectLoader().load_db()
 # SQL statement
-query = "SELECT * FROM cluster WHERE id = 2"
+query = "SELECT * FROM cluster WHERE id = 1"
 # query = "SELECT * FROM cluster"
 db.execute(query)
 
 # Loop through db
 for row in db.cur.fetchall():
 
+    # Load cluster info from db
+    cluster_db = DBInfo(row)
+    name, path = cluster_db.load_cluster()
+    unit_nb = int(cluster_db.unit[-2:])
+    channel_nb = int(cluster_db.channel[-2:])
+    format = cluster_db.format
+    motif = cluster_db.motif
     # Load class object
-    bi = BaselineInfo(row, update=update)
-    mi = MotifInfo(row, update=update)
+    bi = BaselineInfo(path, channel_nb, unit_nb, format, name, update=update)
+    mi = MotifInfo(path, channel_nb, unit_nb, motif, format, name, update=update)  # cluster object
+
 
     # Calculate firing rates
     mi.get_mean_fr()
