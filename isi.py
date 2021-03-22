1c2736cdca5e5fab4514a5e1106392305903b510
Jaerong Ahn <jaerongahn@gmail.com>
2021-03-22 16:53:31 -0400

    script for analyzing isi
    Signed-off-by: Jaerong Ahn <jaerongahn@gmail.com>

--- isi.py
+++ isi.py
@@ -9,50 +9,33 @@ from database.load import ProjectLoader, DBInfo
 import matplotlib.pyplot as plt
 import numpy as np
 from util import save
+from util.draw import remove_right_top
+from util.functions import myround
+import math
 
 
 def print_out_text(ax, peak_latency,
-                   firing_rates,
-                   burst_mean_duration, burst_freq, burst_mean_spk, burst_fraction,
-                   category,
-                   burst_index
+                   ref_prop,
                    ):
-    font_size = 12
     txt_xloc = 0
     txt_yloc = 1
-    txt_inc = 0.15
+    txt_inc = 0.3
     ax.set_ylim([0, 1])
     txt_yloc -= txt_inc
-    ax.text(txt_xloc, txt_yloc, f"Peak Latency = {round(peak_latency, 3)} (ms)", fontsize=font_size)
+    ax.text(txt_xloc, txt_yloc, f"ISI peak latency = {round(peak_latency, 3)} (ms)", fontsize=font_size)
     txt_yloc -= txt_inc
-    ax.text(txt_xloc, txt_yloc, f"Firing Rates = {round(firing_rates, 3)} Hz", fontsize=font_size)
-    txt_yloc -= txt_inc
-    ax.text(txt_xloc, txt_yloc, f'Burst Duration = {round(burst_mean_duration, 3)}', fontsize=font_size)
-    txt_yloc -= txt_inc
-    ax.text(txt_xloc, txt_yloc, f'Burst Freq = {round(burst_freq, 3)} Hz', fontsize=font_size)
-    txt_yloc -= txt_inc
-    ax.text(txt_xloc, txt_yloc, f'Burst MeanSpk = {round(burst_mean_spk, 3)}', fontsize=font_size)
-    txt_yloc -= txt_inc
-    ax.text(txt_xloc, txt_yloc, f'Burst Fraction = {round(burst_fraction, 3)} (%)', fontsize=font_size)
-    txt_yloc -= txt_inc
-    ax.text(txt_xloc, txt_yloc, 'Category = {}'.format(category), fontsize=font_size)
-    txt_yloc -= txt_inc
-    ax.text(txt_xloc, txt_yloc, f'Burst Index = {round(burst_index, 3)}', fontsize=font_size)
+    ax.text(txt_xloc, txt_yloc, f"Within Ref Proportion= {round(ref_prop, 3)} %", fontsize=font_size)
     ax.axis('off')
 
 
 # Parameter
-nb_row = 7
+nb_row = 6
 nb_col = 3
 update = False
 save_fig = True
 update_db = False  # save results to DB
 fig_ext = '.png'  # .png or .pdf
-
 font_size = 12
-txt_xloc = 0
-txt_yloc = 1
-txt_inc = 0.15
 
 
 # Load database
@@ -73,49 +56,36 @@ for row in db.cur.fetchall():
     motif = cluster_db.motif
 
     mi = MotifInfo(path, channel_nb, unit_nb, motif, format, name, update=update)  # cluster object
-    bi = BaselineInfo(path, channel_nb, unit_nb, format, name, update=update)  # bout object
+    bi = BaselineInfo(path, channel_nb, unit_nb, format, name, update=update)  # baseline object
 
     # Get ISI per condition
     isi = mi.get_isi(add_premotor_spk=False)
     isi['B'] = bi.get_isi()
 
-    # Get isi bin size (note that this will be in log scale)
-    isi_win = 4
-    isi_scale = 100
-    hist_bin = np.arange(-1, isi_win, 1 / isi_scale)
-
     # Plot the results
-    fig = plt.figure(figsize=(13, 7))
+    fig = plt.figure(figsize=(11, 5))
     fig.set_dpi(500)
     plt.suptitle(mi.name, y=.95)
 
     if 'B'in isi.keys():
-        ax1 = plt.subplot2grid((nb_row, nb_col), (0, 0), rowspan=3, colspan=1)
-        # ax1.plot(spk_corr_parm['time_bin'], isi['B'], 'Baseline', normalize=normalize)
-        # ax1.plot(spk_corr_parm['time_bin'], isi['B'], 'Baseline', normalize=normalize)
-        # corr_b.plot_corr(ax1, spk_corr_parm['time_bin'], correlogram['B'], 'Baseline', normalize=normalize)
-        # ax_txt1 = plt.subplot2grid((nb_row, nb_col), (4, 0), rowspan=3, colspan=1)
-        # print_out_text(ax_txt1, corr_b.peak_latency, bi.mean_fr,
-        #                burst_info_b.mean_duration, burst_info_b.freq, burst_info_b.mean_nb_spk,
-        #                burst_info_b.fraction, corr_b.category, corr_b.burst_index)
+        ax1 = plt.subplot2grid((nb_row, nb_col), (1, 0), rowspan=3, colspan=1)
+        isi['B'].plot(ax1, 'Log ISI (Baseline)')
+        ax_txt1 = plt.subplot2grid((nb_row, nb_col), (4, 0), rowspan=2, colspan=1)
+        print_out_text(ax_txt1, isi['B'].peak_latency, isi['B'].within_ref_prop)
 
-    # if corr_u:
-    #     ax2 = plt.subplot2grid((nb_row, nb_col), (0, 1), rowspan=3, colspan=1)
-    #     corr_u.plot_corr(ax2, spk_corr_parm['time_bin'], correlogram['U'], 'Undir', normalize=normalize)
-    #     ax_txt2 = plt.subplot2grid((nb_row, nb_col), (4, 1), rowspan=3, colspan=1)
-    #     print_out_text(ax_txt2, corr_u.peak_latency, mi.mean_fr['U'],
-    #                    burst_info_u.mean_duration, burst_info_u.freq, burst_info_u.mean_nb_spk,
-    #                    burst_info_u.fraction, corr_u.category, corr_u.burst_index)
-    #
-    # if corr_d:
-    #     ax3 = plt.subplot2grid((nb_row, nb_col), (0, 2), rowspan=3, colspan=1)
-    #     corr_d.plot_corr(ax3, spk_corr_parm['time_bin'], correlogram['D'], 'Dir', normalize=normalize)
-    #     ax_txt3 = plt.subplot2grid((nb_row, nb_col), (4, 2), rowspan=3, colspan=1)
-    #     print_out_text(ax_txt3, corr_d.peak_latency, mi.mean_fr['D'],
-    #                    burst_info_d.mean_duration, burst_info_d.freq, burst_info_d.mean_nb_spk,
-    #                    burst_info_d.fraction, corr_d.category, corr_d.burst_index)
+    if 'U'in isi.keys():
+        ax2 = plt.subplot2grid((nb_row, nb_col), (1, 1), rowspan=3, colspan=1)
+        isi['U'].plot(ax2, 'Log ISI (Undir)')
+        ax_txt2 = plt.subplot2grid((nb_row, nb_col), (4, 1), rowspan=2, colspan=1)
+        print_out_text(ax_txt2, isi['U'].peak_latency, isi['U'].within_ref_prop)
 
+    if 'D'in isi.keys():
+        ax3 = plt.subplot2grid((nb_row, nb_col), (1, 2), rowspan=3, colspan=1)
+        isi['D'].plot(ax3, 'Log ISI (Dir)')
+        ax_txt3 = plt.subplot2grid((nb_row, nb_col), (4, 2), rowspan=2, colspan=1)
+        print_out_text(ax_txt3, isi['D'].peak_latency, isi['D'].within_ref_prop)
 
+    plt.show()
 
 
     # # Save results to database
@@ -170,9 +140,9 @@ for row in db.cur.fetchall():
     #     db.to_csv('cluster')
     #     print('Done!')
     #
-    # # Save results
-    # if save_fig:
-    #     save_path = save.make_dir(ProjectLoader().path / 'Analysis', 'UnitProfiling')
-    #     save.save_fig(fig, save_path, mi.name, fig_ext=fig_ext)
-    # else:
-    #     plt.show()
+    # Save results
+    if save_fig:
+        save_path = save.make_dir(ProjectLoader().path / 'Analysis', 'UnitProfiling')
+        save.save_fig(fig, save_path, mi.name, fig_ext=fig_ext)
+    else:
+        plt.show()
