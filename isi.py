e137cc0b47b68fa4044dc492b988e0fbaebaa1b6
Jaerong Ahn <jaerongahn@gmail.com>
2021-03-23 11:43:39 -0400

    isi
    Signed-off-by: Jaerong Ahn <jaerongahn@gmail.com>

--- isi.py
+++ isi.py
@@ -7,18 +7,14 @@ Inter-spike interval analysis
 from analysis.spike import MotifInfo, BaselineInfo
 from database.load import ProjectLoader, DBInfo
 import matplotlib.pyplot as plt
-import numpy as np
 from util import save
-from util.draw import remove_right_top
-from util.functions import myround
-import math
 
 
 def print_out_text(ax, peak_latency,
                    ref_prop,
                    ):
     txt_xloc = 0
-    txt_yloc = 1
+    txt_yloc = 0.5
     txt_inc = 0.3
     ax.set_ylim([0, 1])
     txt_yloc -= txt_inc
@@ -85,61 +81,6 @@ for row in db.cur.fetchall():
         ax_txt3 = plt.subplot2grid((nb_row, nb_col), (4, 2), rowspan=2, colspan=1)
         print_out_text(ax_txt3, isi['D'].peak_latency, isi['D'].within_ref_prop)
 
-    plt.show()
-
-
-    # # Save results to database
-    # if update_db:
-    #     # Baseline
-    #     db.create_col('cluster', 'burstDurationBaseline', 'REAL')
-    #     db.update('cluster', 'burstDurationBaseline', row['id'], burst_info_b.mean_duration)
-    #     db.create_col('cluster', 'burstFreqBaseline', 'REAL')
-    #     db.update('cluster', 'burstFreqBaseline', row['id'], burst_info_b.freq)
-    #     db.create_col('cluster', 'burstMeanNbSpkBaseline', 'REAL')
-    #     db.update('cluster', 'burstMeanNbSpkBaseline', row['id'], burst_info_b.mean_nb_spk)
-    #     db.create_col('cluster', 'burstFractionBaseline', 'REAL')
-    #     db.update('cluster', 'burstFractionBaseline', row['id'], burst_info_b.fraction)
-    #     db.create_col('cluster', 'unitCategoryBaseline', 'STRING')
-    #     if corr_b:
-    #         db.update('cluster', 'unitCategoryBaseline', row['id'], str(corr_b.category))
-    #     db.create_col('cluster', 'burstIndexBaseline', 'REAL')
-    #     if corr_b:
-    #         db.update('cluster', 'burstIndexBaseline', row['id'], corr_b.burst_index)
-    #     # Undir
-    #     db.create_col('cluster', 'burstDurationUndir', 'REAL')
-    #     db.update('cluster', 'burstDurationUndir', row['id'], burst_info_u.mean_duration)
-    #     db.create_col('cluster', 'burstFreqUndir', 'REAL')
-    #     db.update('cluster', 'burstFreqUndir', row['id'], burst_info_u.freq)
-    #     db.create_col('cluster', 'burstMeanNbSpkUndir', 'REAL')
-    #     db.update('cluster', 'burstMeanNbSpkUndir', row['id'], burst_info_u.mean_nb_spk)
-    #     db.create_col('cluster', 'burstFractionUndir', 'REAL')
-    #     db.update('cluster', 'burstFractionUndir', row['id'], burst_info_u.fraction)
-    #     db.create_col('cluster', 'unitCategoryUndir', 'STRING')
-    #     if corr_u:
-    #         db.update('cluster', 'unitCategoryUndir', row['id'], str(corr_u.category))
-    #     db.create_col('cluster', 'burstIndexUndir', 'REAL')
-    #     if corr_u:
-    #         db.update('cluster', 'burstIndexUndir', row['id'], corr_u.burst_index)
-    #     # Dir
-    #     db.create_col('cluster', 'burstDurationDir', 'REAL')
-    #     db.update('cluster', 'burstDurationDir', row['id'], burst_info_d.mean_duration)
-    #     db.create_col('cluster', 'burstFreqDir', 'REAL')
-    #     db.update('cluster', 'burstFreqDir', row['id'], burst_info_d.freq)
-    #     db.create_col('cluster', 'burstMeanNbSpkDir', 'REAL')
-    #     db.update('cluster', 'burstMeanNbSpkDir', row['id'], burst_info_d.mean_nb_spk)
-    #     db.create_col('cluster', 'burstFractionDir', 'REAL')
-    #     db.update('cluster', 'burstFractionDir', row['id'], burst_info_d.fraction)
-    #     db.create_col('cluster', 'unitCategoryDir', 'STRING')
-    #     if corr_d:
-    #         db.update('cluster', 'unitCategoryDir', row['id'], str(corr_d.category))
-    #     db.create_col('cluster', 'burstIndexDir', 'REAL')
-    #     if corr_d:
-    #         db.update('cluster', 'burstIndexDir', row['id'], corr_d.burst_index)
-    #
-    #     # Convert db to csv
-    #     db.to_csv('cluster')
-    #     print('Done!')
-    #
     # Save results
     if save_fig:
         save_path = save.make_dir(ProjectLoader().path / 'Analysis', 'UnitProfiling')
