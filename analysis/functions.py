8b06c11ea8772b1c44cffd008677544dea8dfead
Jaerong Ahn <jaerongahn@gmail.com>
2021-03-29 23:50:02 -0400

    added get_pre_motor_spk_per_note
    Signed-off-by: Jaerong Ahn <jaerongahn@gmail.com>

--- analysis/functions.py
+++ analysis/functions.py
@@ -380,3 +380,77 @@ def get_basis_psd(psd_list, notes, num_note_crit_basis=30):
             # plt.show()
     return psd_basis_list, syl_basis_list
 
+
+def get_pre_motor_spk_per_note(ClusterInfo, song_note, save_path, npy_update=False):
+    """
+    Get the number of spikes in the pre-motor window for individual note
+
+    Parameters
+    ----------
+    ClusterInfo : class
+    song_note : str
+        notes to be used for analysis
+    save_path : path
+
+    Returns
+    -------
+    pre_motor_spk_dict : dict
+    """
+    # Get number of spikes from pre-motor window per note
+
+    from database.load import ProjectLoader
+    import numpy as np
+
+    # Create a new database (song_syllable)
+    db = ProjectLoader().load_db()
+    with open('database/create_song_syllable.sql', 'r') as sql_file:
+        db.conn.executescript(sql_file.read())
+
+    cluster_id = int(ClusterInfo.name.split('-')[0])
+
+    # Set save file (.npy)
+    npy_name = ClusterInfo.name + '.npy'
+    npy_name = save_path / npy_name
+
+    if npy_name.exists() and not npy_update:
+        pre_motor_spk_dict = np.load(npy_name,
+                                     allow_pickle=True).item()  # all pre-deafening data to be combined for being used as a template
+    else:
+        nb_pre_motor_spk = np.array([], dtype=np.int)
+        note_onset_ts = np.array([], dtype=np.float32)
+        all_notes = ''
+
+        for onsets, notes, spks in zip(ClusterInfo.onsets, ClusterInfo.syllables,
+                                       ClusterInfo.spk_ts):  # loop through renditions
+            onsets = np.delete(onsets, np.where(onsets == '*'))
+            onsets = np.asarray(list(map(float, onsets)))
+            notes = notes.replace('*', '')
+            for onset, note in zip(onsets, notes):  # loop through notes
+                if note in song_note:
+                    pre_motor_win = [onset - 50, onset]
+                    nb_spk = len(spks[np.where((spks >= pre_motor_win[0]) & (spks <= pre_motor_win[-1]))])
+                    nb_pre_motor_spk = np.append(nb_pre_motor_spk, nb_spk)
+                    note_onset_ts = np.append(note_onset_ts, onset)
+                    all_notes += note
+
+                    # Update database
+                    # query = "INSERT INTO motif_syllable(clusterID, note, nb_premotor_spk) VALUES({}, {}, {})".format(cluster_id, note, nb_spk)
+                    # query = "INSERT INTO motif_syllable(clusterID, note, nbPremotorSpk) VALUES(?, ?, ?)"
+                    # db.cur.execute(query, (cluster_id, note, nb_spk))
+        # db.conn.commit()
+        # Store info in a dictionary
+        pre_motor_spk_dict = {}
+        pre_motor_spk_dict['pre_motor_win'] = 50  # pre-motor window before syllable onset in ms
+
+        for note in unique(all_notes):
+            ind = find_str(all_notes, note)
+            pre_motor_spk_dict[note] = {}  # nested dictionary
+            pre_motor_spk_dict[note]['nb_spk'] = nb_pre_motor_spk[ind]
+            pre_motor_spk_dict[note]['onset_ts'] = note_onset_ts[ind]
+
+        save_path = save.make_dir(ProjectLoader().path / 'Analysis', 'PSD_similarity' + '/' + 'SpkCount',
+                                  add_date=False)
+        npy_name = ClusterInfo.name + '.npy'
+        npy_name = save_path / npy_name
+        np.save(npy_name, pre_motor_spk_dict)
+    return pre_motor_spk_dict
