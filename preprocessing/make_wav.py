0a5f7ce2a2c3888bd89ff9a2231fa03c2575fec7
Jaerong Ahn <jaerongahn@gmail.com>
2021-04-09 00:35:54 -0400

    make_wav
    Signed-off-by: Jaerong Ahn <jaerongahn@gmail.com>

--- preprocessing/make_wav.py
+++ preprocessing/make_wav.py
@@ -2,6 +2,7 @@
 By Jaerong
 Convert audio files with no extension (Mimi's data) into .wav format
 e.g., undir.20120128.1850 -> k27o36_120128_1850_Undir.wav
+undir.20120128.1855.not
 """
 
 from pathlib import Path
@@ -14,11 +15,22 @@ context = 'Undir'
 if data_path is None:
     data_path = find_data_path()
 
-files = [file for file in data_path.rglob('*') if file.suffix != '.mat']  # files to read & convert
+audio_files = [file for file in data_path.rglob('*') if file.suffix != '.mat']  # files to read & convert
+not_mat_files =  [file for file in data_path.rglob('*') if file.suffix == '.mat']
 
-for file in files:
-    print("Processing ..." + file.name)
-    date = file.name.split('.')[1][2:]
-    file_ind = file.name.split('.')[-1]
+for audio_file, not_mat_file in zip(audio_files, not_mat_files):
+    date = audio_file.name.split('.')[1][2:]
+    file_ind = audio_file.name.split('.')[-1]
 
-    new_file_name = '_'.join([bird_id, date, file_ind])  # e.g., k27o36_120330_188
+
+    new_audio_name = '_'.join([bird_id, date, file_ind, context])  # e.g., k27o36_120128_1850_Undir.wav
+    new_audio_name += '.wav'
+    new_not_mat_name = new_audio_name + '.not.mat'
+
+    new_audio_name = audio_file.parent / new_audio_name
+    new_not_mat_name = not_mat_file.parent / new_not_mat_name
+    print(f"Changing '{audio_file.name}' into '{new_audio_name}'")
+    audio_file.rename(new_audio_name)
+    not_mat_file.rename(new_not_mat_name)
+
+print('Done!')
