331e073e4305e2a0ba35232299ecc1188517672a
Jaerong Ahn <jaerongahn@gmail.com>
2021-04-18 00:45:55 -0400

    added collect_spk_info.py
    Signed-off-by: Jaerong Ahn <jaerongahn@gmail.com>

---
+++ preprocessing/collect_spk_info.py
@@ -0,0 +1,26 @@
+"""
+Collect SpkInfo.mat files for spike info analysis
+"""
+
+from database.load import ProjectLoader, DBInfo
+import shutil
+from util import save
+
+# Load database
+db = ProjectLoader().load_db()
+# SQL statement
+query = "SELECT * FROM cluster WHERE ephysOK = 1"
+db.execute(query)
+
+# Loop through db
+for row in db.cur.fetchall():
+
+    # Load cluster info from db
+    cluster_db = DBInfo(row)
+    name, path = cluster_db.load_cluster_db()
+    mat_file = path / 'SpkInfo.mat'
+    new_mat_name = name + '.mat'
+
+    save_path = save.make_dir(ProjectLoader().path / 'Analysis', 'InformationAnalysis', add_date=False)
+    new_file_path = save_path / new_mat_name
+    shutil.copy(mat_file, new_file_path)
--- preprocessing/main.py
+++
@@ -1,6 +0,0 @@
-data_path = "H:\Box\Data\BMI\o54w8\BMI"
-
-import preprocessing.convert
-import preprocessing.plot
-
-
