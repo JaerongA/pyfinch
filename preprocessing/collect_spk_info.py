0bc1787ee87e1552826a884d314290c4140676ce
Jaerong Ahn <jaerongahn@gmail.com>
2021-04-18 23:46:56 -0400

    encapsulation into main function
    Signed-off-by: Jaerong Ahn <jaerongahn@gmail.com>

--- preprocessing/collect_spk_info.py
+++ preprocessing/collect_spk_info.py
@@ -2,25 +2,31 @@
 Collect SpkInfo.mat files for spike info analysis
 """
 
-from database.load import ProjectLoader, DBInfo
-import shutil
-from util import save
+def main():
 
-# Load database
-db = ProjectLoader().load_db()
-# SQL statement
-query = "SELECT * FROM cluster WHERE ephysOK = 1"
-db.execute(query)
+    from database.load import ProjectLoader, DBInfo
+    import shutil
+    from util import save
 
-# Loop through db
-for row in db.cur.fetchall():
+    # Load database
+    db = ProjectLoader().load_db()
+    # SQL statement
+    query = "SELECT * FROM cluster WHERE ephysOK = 1"
+    db.execute(query)
 
-    # Load cluster info from db
-    cluster_db = DBInfo(row)
-    name, path = cluster_db.load_cluster_db()
-    mat_file = path / 'SpkInfo.mat'
-    new_mat_name = name + '.mat'
+    # Loop through db
+    for row in db.cur.fetchall():
 
-    save_path = save.make_dir(ProjectLoader().path / 'Analysis', 'InformationAnalysis', add_date=False)
-    new_file_path = save_path / new_mat_name
-    shutil.copy(mat_file, new_file_path)
+        # Load cluster info from db
+        cluster_db = DBInfo(row)
+        name, path = cluster_db.load_cluster_db()
+        mat_file = path / 'SpkInfo.mat'
+        new_mat_name = name + '.mat'
+
+        save_path = save.make_dir(ProjectLoader().path / 'Analysis', 'InformationAnalysis', add_date=False)
+        new_file_path = save_path / new_mat_name
+        shutil.copy(mat_file, new_file_path)
+
+
+if __name__ == '__main__':
+    main()
