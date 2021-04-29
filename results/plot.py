c6af0d239750143b0dbbf5839ad6ae6dcc23f515
Jaerong Ahn <jaerongahn@gmail.com>
2021-04-28 21:45:38 -0400

    plot_cluster_pie_chart
    Signed-off-by: Jaerong Ahn <jaerongahn@gmail.com>

--- results/plot.py
+++ results/plot.py
@@ -1,8 +1,6 @@
-def get_nb_cluster():
+def get_nb_cluster(ax):
     from database.load import ProjectLoader
     from pandas.plotting import table
-    import matplotlib.pyplot as plt
-    from util import save
 
     # Load database
     db = ProjectLoader().load_db()
@@ -17,59 +15,9 @@ def get_nb_cluster():
     df_nb_cluster.loc['Total'] = df_nb_cluster.sum(numeric_only=True)
     df_nb_cluster = df_nb_cluster[['Predeafening', 'Postdeafening']]
 
-    fig, ax = plt.subplots(1, 1)
-    plt.title("# of clusters")
-    table(ax, df_nb_cluster, loc="center", colWidths=[0.2, 0.2, 0.2]);
-    plt.axis('off')
-    plt.show()
-
-
-def plot_cluster_pie_chart(axis, colors, category_column_name):
-    pass
-
-
-# def plot_bar_comparison():
-# #     # plot a bar chart with a stat test
-# #     pass
-
-
-# get_nb_cluster()
-
-# category_column_name = 'unitCategoryUndir'
-#
-# from database.load import ProjectLoader
-# import matplotlib.pyplot as plt
-#
-# # Load database
-# db = ProjectLoader().load_db()
-# # # SQL statement
-# df = db.to_dataframe("SELECT * FROM cluster WHERE ephysOK=TRUE")
-# df.set_index('id')
-# unit_category = df[category_column_name].dropna()
-#
-# # Plot pie chart
-# explode = (0.1, 0)
-# colors = ['#66b3ff', '#ff9999']
-# values = [sum(unit_category == 'Bursting'), sum(unit_category == 'NonBursting')]
-# fig, ax = plt.subplots()
-# ax.pie(values, explode=explode, colors=colors,
-#         shadow=True, labels=unit_category.unique(), startangle=90,
-#         autopct=lambda p: '{:.2f}%  ({:,.0f})'.format(p, p * sum(values) / 100))
-#
-# plt.title('Cluster Category')
-# ax.axis('equal')
-#
-# plt.show()
-#
-# # summary = summary[summary['UnitCategory_Undir'] == 'Bursting']
-# # labels=UnitCategory.unique()
-# # print(labels)
-
-
-from database.load import ProjectLoader
-import matplotlib.pyplot as plt
-import seaborn as sns
-from util import save
+    # Plot in a table format
+    table(ax, df_nb_cluster,
+          loc="center", colWidths=[0.2, 0.2, 0.2], cellLoc='center');
 
 
 def plot_bar_comparison(ax, dependent_var, group_var, hue_var=None,
@@ -77,99 +25,95 @@ def plot_bar_comparison(ax, dependent_var, group_var, hue_var=None,
                         col_order=None,
                         x_max=0, y_max=None,
                         jitter=0.1, alpha=0.5,
-                        run_stats=False
+                        run_stats=True, stat_txt_size = 10,
+                        legend_ok=False
                         ):
-
     import math
     import numpy as np
+    import matplotlib.pyplot as plt
     from scipy import stats
+    import seaborn as sns
     from util.functions import myround
 
-    dependent_var.replace('', np.nan, inplace=True)  # replace empty cells with np.nans (to prevent the var to be recognized as non-numeric)
+    dependent_var.replace('', np.nan,
+                          inplace=True)  # replace empty cells with np.nans (to prevent the var to be recognized as non-numeric)
 
     ax = sns.stripplot(group_var, dependent_var,
                        size=5, hue=hue_var, jitter=jitter, order=col_order,
                        edgecolor="gray", alpha=alpha, linewidth=1)
 
     ax = sns.barplot(group_var, dependent_var, ax=ax, facecolor=(1, 1, 1, 0),
-                     linewidth=bar_line_width,
+                     linewidth=1,
                      order=col_order, errcolor=".2", edgecolor=".2")
-
+    title += '\n\n\n'
     plt.title(title), plt.xlabel(xlabel), plt.ylabel(ylabel)
 
     # Add stat comparisons
     if run_stats:
-        group1 = dependent_var[group_var == set(group_var)[0]]
-        group2 = dependent_var[group_var == set(group_var)[1]]
-        tval, pval = stats.ttest_ind(group1, group2)
+        group1 = dependent_var[group_var == list(set(group_var))[0]]
+        group2 = dependent_var[group_var == list(set(group_var))[1]]
+        tval, pval = stats.ttest_ind(group1, group2, nan_policy='omit')
         degree_of_freedom = len(group1) + len(group2) - 2
 
-        if pval < 0.001:  sig = '***'
-        elif pval < 0.01:  sig = '**'
-        elif pval < 0.05:  sig = '*'
-        else:  sig = 'ns'
+        if pval < 0.001:
+            sig = '***'
+        elif pval < 0.01:
+            sig = '**'
+        elif pval < 0.05:
+            sig = '*'
+        else:
+            sig = 'ns'
 
         x1, x2 = 0, 1
-        y, h, col = df['motifFRUndir'].max() + 3, 1, 'k'
-        plt.plot([x1, x1, x2, x2], [y, y + h, y + h, y], lw=1.5, c=col)
-        plt.text((x1 + x2) * .5, y + h * 1, sig, ha='center', va='bottom', color=col, size=15)
+        y, h, col = ax.get_ylim()[1] * 1.01, 0.2, 'k'
+        plt.plot([x1, x1, x2, x2], [y, y + h, y + h, y], lw=1, c=col)
+        plt.text((x1 + x2) * .5, y + h * 1, sig, ha='center', va='bottom', color=col, size=stat_txt_size)
         msg = ('$P$ = {:.3f}'.format(pval))
-        plt.text((x1 + x2) * .5, y_max * 1.2, msg, ha='center', va='bottom', color=col, size=stat_txt_size)
+        plt.text((x1 + x2) * .5, y * 1.1, msg, ha='center', va='bottom', color=col, size=stat_txt_size)
         msg = ('t({:.0f})'.format(degree_of_freedom) + ' = {:.2f}'.format(tval))
-        plt.text((x1 + x2) * .5, y_max * 1.3, msg, ha='center', va='bottom', color=col, size=stat_txt_size)
+        plt.text((x1 + x2) * .5, y * 1.2, msg, ha='center', va='bottom', color=col, size=stat_txt_size)
 
     if y_max:
         plt.ylim(x_max, y_max)
     else:
-        ax.set_ylim([0, myround(math.ceil(ax.get_ylim()[1]), base=15)])
+        ax.set_ylim([0, myround(math.ceil(y), base=10)])
     ax.spines['right'].set_visible(False), ax.spines['top'].set_visible(False)
-    ax.get_legend().remove()
+    if legend_ok:
+        plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    else:
+        ax.get_legend().remove()
 
+
+def plot_cluster_pie_chart(axis, colors, category_column_name):
+    pass
+
+
+
+
+from database.load import ProjectLoader
+import matplotlib.pyplot as plt
+from util import save
 
 # Load database
 db = ProjectLoader().load_db()
 # # SQL statement
-df = db.to_dataframe("SELECT * FROM cluster WHERE analysisOK=TRUE")
-df.set_index('id')
+df = db.to_dataframe("SELECT unitCategoryUndir FROM cluster WHERE ephysOK=TRUE")
+unit_category = df['unitCategoryUndir']
 
-# Parameters
-nb_row = 3
-nb_col = 3
-bar_line_width = 2
-stat_txt_size = 12
-save_fig=False
+explode = (0.1, 0)
+colors = ['#66b3ff', '#ff9999']
+values = [sum(unit_category == 'Bursting'), sum(unit_category == 'NonBursting')]
 
-# Plot the results
-fig = plt.figure(figsize=(9, 4))
-plt.subplot(1, 3, 1)
-plt.suptitle('Firing Rates', y=.9, fontsize=20)
+fig, ax = plt.subplots()
+ax.pie(values, explode=explode, colors=colors,
+        shadow=True, labels=unit_category.unique(), startangle=90,
+        autopct=lambda p: '{:.2f}%  ({:,.0f})'.format(p, p * sum(values) / 100))
 
-# Baseline FR
-ax = plt.subplot2grid((nb_row, nb_col), (1, 0), rowspan=2, colspan=1)
-plot_bar_comparison(ax, df['baselineFR'], df['taskName'], hue_var=df['birdID'],
-                    title='Baseline', ylabel='Firing Rates (Hz)',
-                    col_order=("Predeafening", "Postdeafening"),
-                    )
+plt.title('Unit Category (Undir)')
+ax.axis('equal')
 
-# Undir
-ax = plt.subplot2grid((nb_row, nb_col), (1, 1), rowspan=2, colspan=1)
-plot_bar_comparison(ax, df['motifFRUndir'], df['taskName'], hue_var=df['birdID'],
-                    title='Undir',
-                    col_order=("Predeafening", "Postdeafening"),
-                    )
+plt.show()
 
-# Dir
-ax = plt.subplot2grid((nb_row, nb_col), (1, 2), rowspan=2, colspan=1)
-plot_bar_comparison(ax, df['motifFRDir'], df['taskName'], hue_var=df['birdID'],
-                    title='Dir',
-                    col_order=("Predeafening", "Postdeafening"),
-                    )
-
-
-plt.tight_layout()
-# Save results
-if save_fig:
-    save_path = save.make_dir(ProjectLoader().path / 'Analysis', 'Results')
-    save.save_fig(fig, save_path, 'Firing Rates', fig_ext='.png')
-else:
-    plt.show()
+# summary = summary[summary['UnitCategory_Undir'] == 'Bursting']
+# labels=UnitCategory.unique()
+# print(labels)
