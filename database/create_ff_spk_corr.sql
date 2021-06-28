b5e49405419c74374bd0d3688891120ab0dbd56d
Jaerong Ahn <jaerongahn@gmail.com>
2021-06-27 23:41:35 -0400

    ff spk corr database
    Signed-off-by: Jaerong Ahn <jaerongahn@gmail.com>

---
+++ database/create_ff_spk_corr.sql
@@ -0,0 +1,26 @@
+-- DB for FF (fundamental frequency analysis)
+
+CREATE TABLE IF NOT EXISTS ff_spk_corr(
+            id                       INTEGER PRIMARY KEY,
+            clusterID                INTEGER NOT NULL,
+            birdID                   STRING,
+            taskName                 STRING,
+            taskSession              INT,
+            note                     STRING NOT NULL,
+            nbNoteUndir              INT,
+            nbNoteDir                INT,
+            ffMeanUndir              REAL,
+            ffMeanDir                REAL,
+            ffUndirCV                REAL,
+            ffDirCV                  REAL,
+            premotorSpkUndir         REAL,
+            nbSpkUndir               INT,   -- number of spikes in the pre-motor window
+            spkCorrRUndir            REAL,   -- correlation between number of spikes and FF
+            spkCorrPvalSigUndir      BOOL,   -- significance of correlation
+            spkCorrPvalSigDir        BOOL,   -- significance of correlation
+            polarityUndir            STRING,  -- positive vs. negative
+            polarityDir              STRING,  -- positive vs. negative
+            spkCorrRsquareUndir      REAL,
+            spkCorrRsquareDir        REAL,
+            UNIQUE (clusterID, note)
+            );
