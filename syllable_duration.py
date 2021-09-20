d9b3e94070c4ccab4ff2e39ac809985ed43fb4a6
Jaerong Ahn <jaerongahn@gmail.com>
2021-09-20 02:25:19 -0400

    leave only one version syllable duration
    Signed-off-by: Jaerong Ahn <jaerongahn@gmail.com>

--- syllable_duration.py
+++ syllable_duration.py
@@ -1,66 +1,170 @@
-# By Jaerong
-# Syllable duration analysis
-
-import os
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-from summary.read_config import parser
-from summary import load
-
-project_path = load.project(parser)  # find the project folder
-# summary_cluster, nb_cluster = load.summary(parser)  # load cluster summary file
-
-data_path = os.path.join(project_path, r'Analysis\SAP_features')  # the data folder where SAP feature values are stored
-analysis_file = 'SAP(ALL).txt'
-
-os.chdir(data_path)
-df = pd.read_csv(analysis_file, delimiter="\t")
-df = df.query('Context == "Undir"')  # select only Undir trials
+"""
+By Jaerong (2020/06/04)
+Syllable duration for all syllables regardless of it type
+Calculation based on EventInfo.m
+"""
 
 
-def unique(list):
-    # Extract unique strings from the list in the order they appeared
-    seen = set()
-    return [x for x in list if not (x in seen or seen.add(x))]
+def get_duration(query):
+    """
+    Input: SQL query
+    calculate the syllable duration from .not.mat and store the data in .csv
+    """
+    # global save_dir
 
+    from analysis.functions import get_note_type
+    from analysis.song import SongInfo
+    from database.load import ProjectLoader, DBInfo
+    import pandas as pd
+    from util import save
 
-bird_list = unique(df['BirdID'].tolist())
-task_list = unique(df['TaskName'].tolist())
+    # Create save path
+    save_path = save.make_dir(ProjectLoader().path / 'Analysis', 'SyllableDuration')
 
-for bird in bird_list:
-    for task in task_list:
+    # Load song database
+    db = ProjectLoader().load_db()
+    db.execute(query)
 
-        note_list = unique(df['Note'].tolist())
-        # bird = 'b70r38'
-        # task = 'Predeafening'
+    # Loop through db
+    for row in db.cur.fetchall():
+        # Load song info from db
+        song_db = DBInfo(row)
+        name, path = song_db.load_song_db()
 
+        # Load song object
+        si = SongInfo(path, name)
+        print('\nAccessing... ' + si.name)
+
+        # Store results in the dataframe
+        df = pd.DataFrame()
+
+        # Organize results
+        note_types = get_note_type(''.join(si.syllables).replace('*', ''), song_db) # Type of the syllables
+        nb_notes = len(''.join(si.syllables).replace('*', ''))
+        durations = []
+        for duration in [list(map(float, duration[duration != '*'])) for duration in si.durations]:
+            durations += duration[0]
+
+        # Save results to a dataframe
         temp_df = []
-        temp_df = df.loc[(df['BirdID'] == bird) & (df['TaskName'] == task)]
-        note_list = unique(temp_df['Note'].tolist())
+        temp_df = pd.DataFrame({'SongID': [song_db.id] * nb_notes,
+                                'BirdID': [song_db.birdID] * nb_notes,
+                                'TaskName': [song_db.taskName] * nb_notes,
+                                'TaskSession': [song_db.taskSession] * nb_notes,
+                                'TaskSessionDeafening': [song_db.taskSessionDeafening] * nb_notes,
+                                'TaskSessionPostdeafening': [song_db.taskSessionPostDeafening] * nb_notes,
+                                'Context': ''.join([len(syllable.replace('*', '')) * context for syllable, context
+                                                    in zip(si.syllables, si.contexts)]),
+                                'SyllableType': note_types,
+                                'Syllable': list(''.join(si.syllables).replace('*','')),
+                                'Duration': durations,
+                                })
+        df = df.append(temp_df, ignore_index=True)
 
-        title = '-'.join([bird, task])
-        fig = plt.figure(figsize=(5, 4))
-        plt.suptitle(title, size=10)
-        # ax = sns.distplot((temp_df['Duration'], hist= False, kde= True)
-        ax = sns.distplot(temp_df['Duration'], hist=False, kde=True)
-        temp_df.groupby(['Note'])['Duration'].mean()  # mean duration of each note
+        # Save to a file
+        df.index.name = 'Index'
+        outputfile = save_path / 'SyllableDuration.csv'
+        df.to_csv(outputfile, index=True, header=True)  # save the dataframe to .cvs format
+        print('Done!')
 
-        # mark each note
-        mean_dur = list(zip(note_list, temp_df.groupby(['Note'])[
-            'Duration'].mean().to_list()))  # [('a', 236.3033654971783), ('b', 46.64262295081962), ('c', 154.57333333333335), ('d', 114.20039483457349)]
-        for note, dur in mean_dur:
-            plt.axvline(dur, color='k', linestyle='dashed', linewidth=1)
+def load_data(data_file, context='ALL', syl_type='ALL'):
 
-        plt.show()
-        print('Prcessing... {} from Bird {}'.format(task, bird))
+    import pandas as pd
 
-        # ax = sns.scatterplot(x='PCA1', y='PCA2', data=temp_df, hue='Note', size=2)
-        # legend = ax.legend(loc='center left', bbox_to_anchor=(1.05, 0.5))
-        # # legend.texts[-2:] = ''
-        # ax.spines['right'].set_visible(False), ax.spines['top'].set_visible(False)
-        #
-        # # plt.show()
-        # # print(title)
+    # Load syllable duration .csv data
+    df = pd.read_csv(data_file)
 
-# ------------------------------------------------------------------------------------------
+    # Select syllables based on social context
+    if context is 'U':
+        df = df.query('Context == "U"')  # select only Undir
+    elif context is 'D':
+        df = df.query('Context == "D"')  # select only Dir
+
+    # Only select syllables of a particular type
+    if syl_type is 'M':
+        df = df.query('SyllableType == "M"')  # eliminate non-labeled syllables (e.g., 0)
+    elif syl_type is 'C':
+        df = df.query('SyllableType == "C"')  # eliminate non-labeled syllables (e.g., 0)
+    elif syl_type is 'I':
+        df = df.query('SyllableType == "I"')  # eliminate non-labeled syllables (e.g., 0)
+    return df
+
+
+if __name__ == '__main__':
+
+    # Check if the data .csv exists
+    from database.load import ProjectLoader
+    data_file = ProjectLoader().path / 'Analysis' / 'SyllableDuration' / 'SyllableDuration.csv'
+
+    if not data_file.exists():
+        # Get the syllable duration data if it already exists
+        # Load song database
+        query = "SELECT * FROM song WHERE id=1"
+        # query = "SELECT * FROM song WHERE id BETWEEN 1 AND 16"
+        get_duration(query)
+        df = load_data(data_file, context='ALL', syl_type='ALL')
+    else: # Get the syllable duration data if it already exists
+        df = load_data(data_file, context='ALL', syl_type='ALL')
+
+
+    # Plot the results
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+    import IPython
+    from util.functions import unique
+    from util import save
+    from database.load import ProjectLoader
+    fig_ext = '.png'
+
+    bird_list = unique(df['BirdID'].tolist())
+    task_list = unique(df['TaskName'].tolist())
+
+    for bird in bird_list:
+
+        for task in task_list:
+
+            note_list = unique(df['Syllable'].tolist())
+
+            # bird = 'b70r38'
+            # task = 'Predeafening'
+
+            temp_df = []
+            temp_df = df.loc[(df['BirdID'] == bird) & (df['TaskName'] == task)]
+
+            if temp_df.empty:
+                continue
+
+            note_list = unique(temp_df.query('SyllableType == "M"')['Syllable'])  # only motif syllables
+
+            title = '-'.join([bird, task])
+            fig = plt.figure(figsize=(6, 5))
+            plt.suptitle(title, size=10)
+            # ax = sns.distplot((temp_df['Duration'], hist= False, kde= True)
+            ax = sns.kdeplot(temp_df['Duration'], bw=0.05, label='', color='k', linewidth=2)
+            # kde = zip(ax.get_lines()[0].get_duration()[0], ax.get_lines()[0].get_duration()[1])
+            # sns.rugplot(temp_df['Duration'])  # shows ticks
+
+            # https: // stackoverflow.com / questions / 43565892 / python - seaborn - distplot - y - value - corresponding - to - a - given - x - value
+            # TODO: extrapolate value and mark with an arrow
+
+            # mark each note
+            median_dur = list(zip(note_list, temp_df.query('SyllableType == "M"').groupby(['Syllable'])[
+                'Duration'].mean().to_list()))  # [('a', 236.3033654971783), ('b', 46.64262295081962), ('c', 154.57333333333335), ('d', 114.20039483457349)]
+            for note, dur in median_dur:
+                plt.axvline(dur, color='k', linestyle='dashed', linewidth=1)
+                plt.arrow(dur, 5, 0, -1)
+            ax.spines['right'].set_visible(False), ax.spines['top'].set_visible(False)
+            plt.xlabel('Duration (ms)')
+            plt.ylabel('Probability Density')
+            # plt.xlim(0, ceil(max(df.loc[(df['BirdID'] == bird)]['Duration']) / 100) * 100)
+            plt.xlim(0, 300)
+            plt.ylim(0, 0.06)
+
+            # print('Prcessing... {} from Bird {}'.format(task, bird))
+
+            # Save results
+            # save_dir = project_path / 'Analysis' / 'SyllableDuration'
+
+            # Create save path
+            save_path = save.make_dir(ProjectLoader().path / 'Analysis', 'SyllableDuration')
+            save.save_fig(fig, save_path, title, fig_ext=fig_ext)
