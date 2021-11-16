682271aa0d13683e3f314803c81d167c90aa5738
Jaerong Ahn <jaerongahn@gmail.com>
2021-11-15 23:59:42 -0500

    analyze song

--- deafening/results/song.py
+++ deafening/results/song.py
@@ -1,55 +1,10 @@
 """
-By Jaerong
-Song analysis
+Plot results from analysis/song.py
 """
-
-def analyze_song(query, update_cache=False, update_db=True):
-
-    from analysis.song import SongInfo
-    from database.load import ProjectLoader, DBInfo
-
-    # Load database
-    db = ProjectLoader().load_db()
-    with open('database/create_song_table.sql', 'r') as sql_file:
-        db.conn.executescript(sql_file.read())
-
-    db.execute(query)
-
-    # Loop through db
-    for row in db.cur.fetchall():
-        # Load song info from db
-        song_db = DBInfo(row)
-        name, path = song_db.load_song_db()
-
-        si = SongInfo(path, name, update=update_cache)  # song object
-
-        nb_files = si.nb_files
-        nb_bouts = si.nb_bouts(song_db.songNote)
-        nb_motifs = si.nb_motifs(song_db.motif)
-
-        mean_nb_intro_notes = si.mean_nb_intro(song_db.introNotes, song_db.songNote)
-        song_call_prop = si.song_call_prop(song_db.calls, song_db.songNote)
-        mi = si.get_motif_info(song_db.motif)  # Get motif info
-        motif_dur = mi.get_motif_duration()  # Get mean motif duration &  CV per context
-
-        if update_db:
-            db.cur.execute("UPDATE song SET nbFilesUndir=?, nbFilesDir=? WHERE id=?", (nb_files['U'], nb_files['D'], song_db.id))
-            db.cur.execute("UPDATE song SET nbBoutsUndir=?, nbBoutsDir=? WHERE id=?", (nb_bouts['U'], nb_bouts['D'], song_db.id))
-            db.cur.execute("UPDATE song SET nbMotifsUndir=?, nbMotifsDir=? WHERE id=?", (nb_motifs['U'], nb_motifs['D'], song_db.id))
-            db.cur.execute("UPDATE song SET meanIntroUndir=?, meanIntroDir=? WHERE id=?", (mean_nb_intro_notes['U'], mean_nb_intro_notes['D'], song_db.id))
-            db.cur.execute("UPDATE song SET songCallPropUndir=?, songCallPropDir=? WHERE id=?", (song_call_prop['U'], song_call_prop['D'], song_db.id))
-            db.cur.execute("UPDATE song SET motifDurationUndir=?, motifDurationDir=? WHERE id=?", (motif_dur['mean']['U'], motif_dur['mean']['D'], song_db.id))
-            db.cur.execute("UPDATE song SET motifDurationCVUndir=?, motifDurationCVDir=? WHERE id=?", (motif_dur['cv']['U'], motif_dur['cv']['D'], song_db.id))
-            db.conn.commit()
-        else:
-            print(nb_files, nb_bouts, nb_motifs, mean_nb_intro_notes, song_call_prop, motif_dur)
-
-    if update_db:
-        db.to_csv(f'song')
-    print('Done!')
+# from deafening.plot import plot_across_days_per_note
 
 
-def plot_across_days(x, y,
+def plot_across_days(df, x, y,
                      context,
                      nb_bout_crit=10,
                      title=None,
@@ -58,17 +13,11 @@ def plot_across_days(x, y,
                      fig_ext='.png',
                      save_fig=False):
 
-    from database.load import ProjectLoader
     import matplotlib.pyplot as plt
     import seaborn as sns
     from util import save
     from util.draw import remove_right_top
 
-    # Load database
-    db = ProjectLoader().load_db()
-    # SQL statement
-    df = db.to_dataframe(f"SELECT * FROM song WHERE nbBoutsUndir >= {nb_bout_crit}")
-    df.set_index('id')
 
     # Plot the results
     circ_size = 0.5
@@ -105,7 +54,7 @@ def plot_across_days(x, y,
 
     # Save figure
     if save_fig:
-        save_path = save.make_dir(ProjectLoader().path / 'Analysis', 'deafening/results')
+        save_path = save.make_dir(ProjectLoader().path / 'Analysis', '')
         save.save_fig(fig, save_path, title, fig_ext=fig_ext, view_folder=False)
     else:
         plt.show()
@@ -159,51 +108,53 @@ def plot_across_days(x, y,
 #     fig.tight_layout()
 #     plt.show()
 
+from database.load import ProjectLoader
 
 # Parameters
 fig_ext = '.png'
 save_fig = False
-update_db = True
-update_cache = False
 nb_bout_crit = 10
 
+title = f"Mean_nb_intro_notes (nb of bouts >= {nb_bout_crit}) Undir"
+
 # SQL statement
-# query = "SELECT * FROM song WHERE id<=13"
-# query = "SELECT * FROM song"
 query = f"SELECT * FROM song WHERE nbBoutsUndir >= {nb_bout_crit}"
 
-# analyze_song(query, update_cache = update_cache, update_db = update_db )
+# Load database
+db = ProjectLoader().load_db()
+# SQL statement
+df = db.to_dataframe(f"SELECT * FROM song WHERE nbBoutsUndir >= {nb_bout_crit}")
 
-# plot_across_days_per_note('taskSessionDeafening', 'meanIntroUndir', 'Undir',
-#                  nb_bout_crit=nb_bout_crit,
-#                  title='Mean_nb_intro_notes',
-#                  x_lim=[-30, 70],
-#                  y_lim=[0, 8],
-#                  fig_ext=fig_ext,
-#                  save_fig=save_fig)
+plot_across_days(df, 'taskSessionDeafening', 'meanIntroUndir', 'Undir',
+                 nb_bout_crit=nb_bout_crit,
+                 title='Mean_nb_intro_notes',
+                 x_lim=[-30, 70],
+                 y_lim=[0, 8],
+                 fig_ext=fig_ext,
+                 save_fig=save_fig)
 
-# plot_across_days_per_note('taskSessionDeafening', 'songCallPropUndir', 'Undir',
-#                  nb_bout_crit=nb_bout_crit,
-#                  title='Song_Call_Proportions',
-#                  x_lim=[-30, 70],
-#                  y_lim=[0, 0.6],
-#                  fig_ext=fig_ext,
-#                  save_fig=save_fig)
-#
-# plot_across_days_per_note('taskSessionDeafening', 'motifDurationUndir', 'Undir',
-#                  nb_bout_crit=nb_bout_crit,
-#                  title='Motif Duration (ms)',
-#                  x_lim=[-30, 70],
-#                  fig_ext=fig_ext,
-#                  save_fig=save_fig)
+plot_across_days(df, 'taskSessionDeafening', 'songCallPropUndir', 'Undir',
+                 nb_bout_crit=nb_bout_crit,
+                 title='Song_Call_Proportions',
+                 x_lim=[-30, 70],
+                 y_lim=[0, 0.6],
+                 fig_ext=fig_ext,
+                 save_fig=save_fig)
 
-# plot_across_days_per_note('taskSessionDeafening', 'motifDurationCVUndir', 'Undir',
-#                  nb_bout_crit=nb_bout_crit,
-#                  title='CV of Motif',
-#                  x_lim=[-30, 70],
-#                  y_lim=[0, 0.04],
-#                  fig_ext=fig_ext,
-#                  save_fig=save_fig)
+plot_across_days(df, 'taskSessionDeafening', 'motifDurationUndir', 'Undir',
+                 nb_bout_crit=nb_bout_crit,
+                 title='Motif Duration (ms)',
+                 x_lim=[-30, 70],
+                 fig_ext=fig_ext,
+                 save_fig=save_fig)
+
+plot_across_days(df, 'taskSessionDeafening', 'motifDurationCVUndir', 'Undir',
+                 nb_bout_crit=nb_bout_crit,
+                 title='CV of Motif',
+                 x_lim=[-30, 70],
+                 y_lim=[0, 0.04],
+                 fig_ext=fig_ext,
+                 save_fig=save_fig)
 
 # pre_post_comparison(query, 'taskName',
 #                     'meanIntroUndir',
