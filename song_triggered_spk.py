3ea2b86c9701d4b68fb6713b6ce83885d31dd1e4
Jaerong Ahn <jaerongahn@gmail.com>
2021-12-21 03:17:42 -0500

    added peth

--- song_triggered_spk.py
+++ song_triggered_spk.py
@@ -8,6 +8,8 @@ from util import save
 from util.draw import remove_right_top
 import warnings
 
+from util.functions import myround
+
 warnings.filterwarnings('ignore')
 
 
@@ -23,13 +25,12 @@ def _get_sorted_notes(target_note) -> str:
     return ''.join(sorted_notes)
 
 
-def _sort_trials_by_dur_per_note(note):
+def _sort_trials_by_dur_per_note(NoteInfo):
     """
     Sort trials based on duration per note
     Parameters
     ----------
-    note : str
-        target note to extract info
+    NoteInfo : class object
 
     Returns
     -------
@@ -42,11 +43,10 @@ def _sort_trials_by_dur_per_note(note):
     note_all = ''  # all song notes in the session
 
     # Extract the info
-    ni = ci.get_note_info(note, pre_buffer=pre_buffer, post_buffer=post_buffer)
-    note_all += ni.note * len(ni.contexts)
+    note_all += NoteInfo.note * len(NoteInfo.contexts)
 
     # Zip lists & filter & sort
-    zipped_list = list(zip(ni.contexts, note_all, ni.spk_ts, ni.onsets, ni.durations))
+    zipped_list = list(zip(NoteInfo.contexts, note_all, NoteInfo.spk_ts, NoteInfo.onsets, NoteInfo.durations))
     zipped_list = list(filter(lambda x: x[0] == context, zipped_list))  # filter context
     if sort_by_duration:
         zipped_list = sorted(zipped_list, key=lambda x: x[-1])  # sort by duration
@@ -57,9 +57,11 @@ def _sort_trials_by_dur_per_note(note):
 
 
 # parameters
-pre_buffer = -50  # in ms
-post_buffer = 300  # in ms
-target_note = None  # all syllables by default
+pre_evt_buffer = 50  # before syllable onset (in ms)
+post_evt_buffer = 300  # after syllable onset (in ms)
+bin_size = 5  # in ms
+nb_bins = abs(pre_evt_buffer) + (2 * abs(post_evt_buffer))
+target_note = None  # set to None to run across all syllables
 marker_size = 2  # to mark the syllable offset  (5 for single syllable, 2 for all syllables)
 context = 'U'
 sort_by_duration = True
@@ -91,6 +93,7 @@ note_contexts = ''
 note_spks = []
 note_onsets = np.array([], dtype='float32')
 note_durations = np.array([], dtype='float32')
+peth = np.array([], dtype=np.float32)
 if not target_note:
     # target_note = cluster_db.introNotes + cluster_db.songNote + cluster_db.calls
     # target_note = cluster_db.introNotes + cluster_db.calls
@@ -100,25 +103,38 @@ if sort_by_syllable:
     # Sort the notes based on median duration
     target_note = _get_sorted_notes(target_note)
 
-    for note in target_note:
-        contexts, notes, spk_ts, onsets, durations = _sort_trials_by_dur_per_note(note)
+    for ind, note in enumerate(target_note):
+        ni = ci.get_note_info(note, pre_buffer=pre_evt_buffer, post_buffer=post_evt_buffer)
+        contexts, notes, spk_ts, onsets, durations = _sort_trials_by_dur_per_note(ni)
 
         note_contexts += contexts
         note_all += notes
         note_spks.extend(spk_ts)
         note_onsets = np.append(note_onsets, onsets)
         note_durations = np.append(note_durations, durations)
+        pi = ni.get_note_peth(time_warp=False, pre_evt_buffer=pre_evt_buffer, duration=post_evt_buffer,
+                              bin_size=bin_size, nb_bins=nb_bins)
+        if ind == 0:
+            peth = pi.peth[context]
+        else:
+            peth = np.concatenate([peth, pi.peth[context]], axis=0)
 
     zipped_list = list(zip(note_contexts, note_all, note_spks, note_onsets, note_durations))
 
 else:
-    for note in target_note:
-        ni = ci.get_note_info(note, pre_buffer=pre_buffer, post_buffer=post_buffer)
+    for ind, note in enumerate(target_note):
+        ni = ci.get_note_info(note, pre_buffer=pre_evt_buffer, post_buffer=post_evt_buffer)
         note_all += ni.note * len(ni.contexts)
         note_contexts += ni.contexts
         note_spks.extend(ni.spk_ts)
         note_onsets = np.append(note_onsets, ni.onsets)
         note_durations = np.append(note_durations, ni.durations)
+        pi = ni.get_note_peth(time_warp=False, pre_evt_buffer=pre_evt_buffer, duration=post_evt_buffer,
+                              bin_size=bin_size, nb_bins=nb_bins)
+        if ind == 0:
+            peth = pi.peth[context]
+        else:
+            peth = np.concatenate([peth, pi.peth[context]], axis=0)
 
     # Zip lists & filter & sort
     zipped_list = list(zip(note_contexts, note_all, note_spks, note_onsets, note_durations))
@@ -126,17 +142,18 @@ else:
     if sort_by_duration:
         zipped_list = sorted(zipped_list, key=lambda x: x[-1])  # sort by duration
 
+
 # Plot figure
-fig = plt.figure(figsize=(6, 7))
+fig = plt.figure(figsize=(6, 8), dpi=600)
 fig.set_tight_layout(False)
 
 fig_name = f"{ci.name} ({target_note}) - {context}"
 plt.suptitle(fig_name, y=.93, fontsize=10)
-gs = gridspec.GridSpec(7, 7)
+gs = gridspec.GridSpec(8, 7)
 gs.update(wspace=0.025, hspace=0.05)
 
 # Plot raster
-ax_raster = plt.subplot(gs[0:7, 0:5])
+ax_raster = plt.subplot(gs[0:6, 0:5])
 line_offsets = np.arange(0.5, note_contexts.count(context))
 
 for note_ind, (_, note, spk_ts, onset, duration) in enumerate(zipped_list):
@@ -147,32 +164,6 @@ for note_ind, (_, note, spk_ts, onset, duration) in enumerate(zipped_list):
                    marker='o', color=color_map[note], markersize=marker_size, alpha=0.5, label=note)
 
 ax_raster.axvline(x=0, linewidth=1, color='r', label='syllable onset', ls='--')
-
-# Print out corr metrics on the figure
-txt_xloc = -0.5
-txt_yloc = 1
-txt_inc = 0.5  # y-distance between texts within the same section
-
-pcc_undir = pcc_dir = max_cross_corr = peak_latency = np.nan
-db.execute(f"SELECT crossCorrMax, peakLatency from song_fr_cross_corr WHERE clusterID= {cluster_db.id}")
-for row in db.cur.fetchall():
-    max_cross_corr, peak_latency = row[0], row[1]
-
-db.execute(f"SELECT pccUndir, pccDir from pcc WHERE clusterID= {cluster_db.id}")
-for row in db.cur.fetchall():
-    pcc_undir, pcc_dir = row[0], row[1]
-ax_txt = plt.subplot(gs[1, -1])
-ax_txt.set_axis_off()
-ax_txt.text(txt_xloc, txt_yloc, f"Cross-corr max = {max_cross_corr : 0.3f}", fontsize=8)
-txt_yloc -= txt_inc
-
-ax_txt.text(txt_xloc, txt_yloc, f"Peak latency = {int(peak_latency)} (ms)", fontsize=8)
-txt_yloc -= txt_inc
-if context == 'U':
-    ax_txt.text(txt_xloc, txt_yloc, f"PCC = {pcc_undir : .3f}", fontsize=8)
-else:
-    ax_txt.text(txt_xloc, txt_yloc, f"PCC = {pcc_dir : .3f}", fontsize=8)
-txt_yloc -= txt_inc
 
 def _print_legend():
     # print out legend
@@ -193,10 +184,50 @@ _print_legend()
 
 remove_right_top(ax_raster)
 ax_raster.set_ylim([0, note_contexts.count(context) + 2])
-ax_raster.set_xlim([pre_buffer, post_buffer])
-ax_raster.set_xlabel('Time (ms)')
+ax_raster.set_xlim([-pre_evt_buffer, post_evt_buffer])
 ax_raster.set_ylabel('Renditions')
+
+# Plot PETH
+ax_peth = plt.subplot(gs[7, 0:5], sharex=ax_raster)
+if len(target_note) > 1:
+    ax_peth.bar(pi.time_bin, peth.sum(axis=0), color='k', width=bin_size)
+else:
+    ax_peth.bar(pi.time_bin, peth.sum(axis=0), color=color_map[note], width=bin_size)
+remove_right_top(ax_peth)
+ax_peth.axvline(x=0, linewidth=1, color='r', ls='--')
+ax_peth.set_ylim(0, myround(ax_peth.get_ylim()[1], base=10))
+ax_peth.set_xlabel('Time (ms)')
+ax_peth.set_ylabel('# of Spk')
+
+
+# Print out corr metrics on the figure
+txt_xloc = -0.5
+txt_yloc = 1.5
+txt_inc = 0.5  # y-distance between texts within the same section
+
+pcc_undir = pcc_dir = max_cross_corr = peak_latency = np.nan
+db.execute(f"SELECT crossCorrMax, peakLatency from song_fr_cross_corr WHERE clusterID= {cluster_db.id}")
+for row in db.cur.fetchall():
+    max_cross_corr, peak_latency = row[0], row[1]
+
+# Run raster.py if pcc table is missing
+db.execute(f"SELECT pccUndir, pccDir from pcc WHERE clusterID= {cluster_db.id}")
+for row in db.cur.fetchall():
+    pcc_undir, pcc_dir = row[0], row[1]
+
+ax_txt = plt.subplot(gs[1, -1])
+ax_txt.set_axis_off()
+ax_txt.text(txt_xloc, txt_yloc, f"Cross-corr max = {max_cross_corr : 0.3f}", fontsize=8)
+txt_yloc -= txt_inc
+
+ax_txt.text(txt_xloc, txt_yloc, f"Peak latency = {int(peak_latency)} (ms)", fontsize=8)
+txt_yloc -= txt_inc
+if context == 'U':
+    ax_txt.text(txt_xloc, txt_yloc, f"Motif PCC = {pcc_undir : .3f}", fontsize=8)
+else:
+    ax_txt.text(txt_xloc, txt_yloc, f"Motif PCC = {pcc_dir : .3f}", fontsize=8)
+txt_yloc -= txt_inc
+fig.tight_layout()
 plt.show()
 
 
-##TODO : add psth
