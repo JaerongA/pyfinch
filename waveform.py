118e5ff38d03ae8ae807ed7642b92da46a6db44f
Jaerong Ahn <jaerongahn@gmail.com>
2021-12-30 23:46:55 -0500

    update

--- waveform.py
+++ waveform.py
@@ -1,77 +1,102 @@
 """
-Plots spike waveform
+Analyze spike waveform metrics
 Calculates signal-to-noise ratio (SNR) relative to the background (raw neural trace)
+Save results to unit_profile table
 """
 from analysis.functions import get_snr
 from analysis.spike import ClusterInfo, NeuralData
-from util import save
-from util.draw import *
 from database.load import ProjectLoader, DBInfo
+import matplotlib.pyplot as plt
+import numpy as np
+from util import save
+from util.draw import remove_right_top, set_fig_size
 
 
-def plot_waveform(axis, wf_ts, spk_wf,
+def plot_waveform(ax, wf_ts, spk_wf,
                   wf_ts_interp=None,
                   avg_wf_interp=None,
-                  spk_proportion=None,
-                  deflection_range=None,
+                  spk_proportion=0.2,
+                  deflection_points=None,
                   avg_wf=True,
                   scale_bar=True
                   ):
-    import numpy as np
-    import matplotlib.pyplot as plt
+    """
+    Plot individual & avg waveforms
+    Parameters
+    ----------
+    ax
+    wf_ts
+    spk_wf
+    wf_ts_interp
+    avg_wf_interp
+    spk_proportion
+    deflection_points : list
+        index of deflection point of a waveform
+    avg_wf : bool
+        overlay averaged waveform
+    scale_bar : bool
+        plot the scale bar
 
-    if not spk_proportion:
-        from analysis.parameters import spk_proportion
-        spk_proportion = spk_proportion
+    """
 
     # Randomly select proportions of waveforms to plot
     np.random.seed(seed=42)
     wf_to_plot = spk_wf[np.random.choice(spk_wf.shape[0], size=int(spk_wf.shape[0] * spk_proportion), replace=False)]
 
     for wf in wf_to_plot:
-        axis.plot(wf_ts, wf, color='k', lw=0.2)
+        ax.plot(wf_ts, wf, color='k', lw=0.2)
 
-    remove_right_top(axis)
+    remove_right_top(ax)
 
     if avg_wf:
         if wf_ts_interp and avg_wf_interp:
-            axis.plot(wf_ts_interp, avg_wf_interp, color='r', lw=2)  # indicate the avg waveform
+            ax.plot(wf_ts_interp, avg_wf_interp, color='r', lw=2)  # indicate the avg waveform
         else:
-            axis.plot(wf_ts, np.nanmean(spk_wf, axis=0), color='r', lw=2)  # indicate the avg waveform
+            ax.plot(wf_ts, np.nanmean(spk_wf, axis=0), color='r', lw=2)  # indicate the avg waveform
 
-    axis.set_xlabel('Time (ms)')
-    axis.set_ylabel('Amplitude (µV)')
-    plt.xlim([-0.2, 1])
+    ax.set_xlabel('Time (ms)')
+    ax.set_ylabel('Amplitude (µV)')
+    ax.set_xlim([-0.2, 1])
 
-    if bool(deflection_range):
-        for ind in deflection_range:
+    if bool(deflection_points):
+        for ind in deflection_points:
             if wf_ts_interp:
-                axis.axvline(x=wf_ts_interp[ind], color='r', linewidth=1, ls='--')
+                ax.axvline(x=wf_ts_interp[ind], color='r', linewidth=1, ls='--')
             else:
-                axis.axvline(x=wf_ts[ind], color='r', linewidth=1, ls='--')
+                ax.axvline(x=wf_ts[ind], color='r', linewidth=1, ls='--')
 
     if scale_bar:
         # Plot a scale bar
-        plt.plot([-0.1, -0.1], [-250, 250], 'k', lw=2)  # for amplitude
-        plt.text(-0.25, -120, '500 µV', rotation=90)
-        plt.plot([0, 0.5], [ax.get_ylim()[0], ax.get_ylim()[0]], 'k', lw=2)  # for time
-        plt.text(0.15, ax.get_ylim()[0] * 1.05, '500 µs')
-        plt.axis('off')
+        ax.plot([-0.1, -0.1], [-250, 250], 'k', lw=2)  # for amplitude
+        ax.text(-0.25, -120, '500 µV', rotation=90)
+        ax.plot([0, 0.5], [ax.get_ylim()[0], ax.get_ylim()[0]], 'k', lw=2)  # for time
+        ax.text(0.15, ax.get_ylim()[0] * 1.05, '500 µs')
+        ax.axis('off')
+
+def analyze_waveform():
+    pass
+
+
+
 
 
 # Parameters
-save_fig = False
-update_db = False
+save_fig = True
+update_db = True
 update = False
-save_wf_values = False
-save_folder_name = 'WaveformAnalysis'
+save_folder_name = 'Waveform'
 fig_ext = '.png'  # .png or .pdf
+font_size = 10
+spk_proportion = 0.5  # proportion of waveforms to plot
+interpolate = False  # interpolate spike waveform to artificially increase the number of data points
+align_wf = True
+filter_crit = 5  # neural data envelope threshold (in s.d), filter out values exceeding this threshold
 
 # Load database
 db = ProjectLoader().load_db()
 
 # SQL statement
-query = "SELECT * FROM cluster WHERE id = 96"
+query = "SELECT * FROM cluster WHERE analysisOK"
 db.execute(query)
 
 # Loop through db
@@ -85,43 +110,38 @@ for row in db.cur.fetchall():
     format = cluster_db.format
 
     ci = ClusterInfo(path, channel_nb, unit_nb, format, name, update=update)  # cluster object
-    ci.analyze_waveform(interpolate=False)  # get waveform features
+    ci.analyze_waveform(interpolate=False, align_wf=align_wf)  # get waveform features
     nd = NeuralData(path, channel_nb, format, update=update)  # raw neural data
 
     # Calculate the SNR (signal-to-noise ratio in dB)
     # variance of the signal (waveform) divided by the variance of the total neural trace
-    snr = get_snr(ci.avg_wf, nd.data)
+    snr = get_snr(ci.avg_wf, nd.data, filter_crit=filter_crit)
 
     # Plot the individual waveforms
     fig = plt.figure(figsize=(7, 5))
     fig.suptitle(ci.name)
     ax = plt.subplot(121)
     plot_waveform(ax, wf_ts=ci.wf_ts, spk_wf=ci.spk_wf,
-                  deflection_range=ci.deflection_range  # demarcate the deflection point
+                  spk_proportion=spk_proportion
                   )
 
     # Print out text
     plt.subplot(122)
     plt.axis('off')
-    plt.text(0.1, 0.8, 'SNR = {} dB'.format(snr), fontsize=12)
-    plt.text(0.1, 0.6, 'Spk Height = {:.2f} µV'.format(ci.spk_height), fontsize=12)
-    plt.text(0.1, 0.4, 'Spk Width = {:.2f} µs'.format(ci.spk_width), fontsize=12)
-    plt.text(0.1, 0.2, 'Half width = {:.2f} µs'.format(ci.half_width), fontsize=12)  # measured from the peak deflection
-    plt.text(0.1, 0.0, '# of Spk = {}'.format(ci.nb_spk), fontsize=12)
+    plt.text(0.1, 0.8, 'SNR = {} dB'.format(snr), fontsize=font_size)
+    plt.text(0.1, 0.6, 'Spk Height = {:.2f} µV'.format(ci.spk_height), fontsize=font_size)
+    plt.text(0.1, 0.4, 'Spk Width = {:.2f} µs'.format(ci.spk_width), fontsize=font_size)
+    plt.text(0.1, 0.2, '# of Spk = {}'.format(ci.nb_spk), fontsize=font_size)
+    plt.text(0.1, 0.0, f"Proportion = {int(spk_proportion * 100)} %" , fontsize=font_size)
     set_fig_size(4.2, 2.5)  # set the physical size of the save_fig in inches (width, height)
 
     # Save results to database
     if update_db:
-        db.create_col('cluster', 'nbSpk', 'INT')
-        db.update('cluster', 'nbSpk', row['id'], ci.nb_spk)
-        db.create_col('cluster', 'SNR', 'REAL')
-        db.update('cluster', 'SNR', row['id'], snr)
-        db.create_col('cluster', 'spkHeight', 'REAL')
-        db.update('cluster', 'spkHeight', row['id'], ci.spk_height)
-        db.create_col('cluster', 'spkWidth', 'REAL')
-        db.update('cluster', 'spkWidth', row['id'], ci.spk_width)
-        db.create_col('cluster', 'spkHalfWidth', 'REAL')
-        db.update('cluster', 'spkHalfWidth', row['id'], ci.half_width)
+        db.cur.execute(f"UPDATE unit_profile SET nbSpk=({ci.nb_spk}) WHERE clusterID=({cluster_db.id})")
+        db.cur.execute(f"UPDATE unit_profile SET spkHeight=({ci.spk_height}) WHERE clusterID=({cluster_db.id})")
+        db.cur.execute(f"UPDATE unit_profile SET spkWidth=({ci.spk_width}) WHERE clusterID=({cluster_db.id})")
+        db.cur.execute(f"UPDATE unit_profile SET SNR=({snr}) WHERE clusterID=({cluster_db.id})")
+        db.conn.commit()
 
     # Save results
     if save_fig:
@@ -129,14 +149,6 @@ for row in db.cur.fetchall():
         save.save_fig(fig, save_path, ci.name, fig_ext=fig_ext)
     else:
         plt.show()
-
-    # Print avg_spk_wf to csv
-    # if save_wf_values:
-    #     import pandas as pd
-    #     df = pd.DataFrame(ci.avg_wf_interp)
-    #     df.to_csv(r'C:\Users\dreill03\Box\AreaX\Analysis\WaveformAnalysis\AverageWaveforms\ave_wf.csv')
-    # else:
-    #     print(ci.avg_wf_interp)
 
 # Convert db to csv
 if update_db:
