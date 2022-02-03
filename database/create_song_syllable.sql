6a27c964ca966685d36eede0a057caf3c8d4d0f7
Jaerong Ahn <jaerongahn@gmail.com>
2022-02-02 23:59:59 -0500

    moved database sql files deafening folders

--- database/create_song_syllable.sql
+++
@@ -1,9 +0,0 @@
--- Store results from raster_syllable
-CREATE TABLE IF NOT EXISTS song_syllable(
-            syllableID      INTEGER PRIMARY KEY,
-            clusterID       INTEGER NOT NULL,
-            note            STRING,
-            nbPremotorSpk INTEGER,
-            psdSimilarity  REAL,
-            FOREIGN KEY(clusterID) REFERENCES cluster(id)
-                                   )
