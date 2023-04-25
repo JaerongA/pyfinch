fbcf103e5980e21e5dfc7be74b65b72fd06374a3
JaerongA <jaerong.ahn@datajoint.com>
2023-04-25 01:20:21 -0500

    deprecate requirements.txt

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 from setuptools import find_packages, setup
 
-# Load the README file.
 with open(file="README.md", mode="r") as readme_handle:
     long_description = readme_handle.read()
-
+    
+with open("version.py") as version_file:
+    exec(version_file.read())
+    
 classifiers = [
     "Development Status :: 3 - Alpha",
     "Intended Audience :: Science/Research",
@@ -12,13 +14,9 @@ classifiers = [
     "Operating System :: OS Independent",
 ]
 
-# specify installation requirements
-with open("requirements.txt") as requirements_file:
-    install_requires = requirements_file.read().splitlines()
-
 setup(
     name="pyfinch",
-    version="0.1.2",
+    version=__version__,
     description="A python package for analyzing neural & bioacoustics signals from songbirds",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -26,10 +24,27 @@ setup(
     author="Jaerong Ahn",
     author_email="jaerongahn@gmail.com",
     license="BSD 3-Clause License",
-    packages=find_packages(),
+    packages=['pyfinch'],
     include_package_data=True,
-    install_requires=install_requires,
     python_requires=">=3.7",
     keywords=["python", "songbird", "zebra finch", "neuroscience", "bioacoustics"],
     classifiers=classifiers,
+    # Load package dependencies from environment.yml file.
+    # Note that this requires the `conda-pack` package to be installed.
+    # Alternatively, you can use `conda env export --file environment.yml`
+    # to generate the environment.yml file and manually update the `setup.py` file.
+    setup_requires=['conda-pack'],
+    install_requires=['conda-pack'],
+    extras_require={
+        'environment': [
+            'conda-build',
+            'conda-verify'
+        ]
+    },
+    zip_safe=False,
+    options={
+        'conda': {
+            'yaml_file': 'environment.yml'
+        }
+    },
 )
